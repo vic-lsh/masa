be1a1d57a28fbb4064ac3c18fae53944fc487a4e
Weixin Deng <weixin@cs.washington.edu>
2024-05-24 00:30:44 +0000

    chore: Add deadline hint to execute API

--- 3rd_party/hyper/src/client/client.rs
+++ 3rd_party/hyper/src/client/client.rs
@@ -19,6 +19,7 @@ use crate::client::connect::CaptureConnectionExtension;
 use crate::common::{exec::BoxSendFuture, lazy as hyper_lazy, sync_wrapper::SyncWrapper, Lazy};
 #[cfg(feature = "http2")]
 use crate::ext::Protocol;
+use crate::rt::DeadlineHint;
 use crate::rt::Executor;
 
 use super::conn;
@@ -339,14 +340,18 @@ where
             });
 
             #[cfg_attr(feature = "deprecated", allow(deprecated))]
-            self.conn_builder.exec.execute(on_idle);
+            self.conn_builder
+                .exec
+                .execute(on_idle, DeadlineHint::Background);
         } else {
             // There's no body to delay, but the connection isn't
             // ready yet. Only re-insert when it's ready
             let on_idle = future::poll_fn(move |cx| pooled.poll_ready(cx)).map(|_| ());
 
             #[cfg_attr(feature = "deprecated", allow(deprecated))]
-            self.conn_builder.exec.execute(on_idle);
+            self.conn_builder
+                .exec
+                .execute(on_idle, DeadlineHint::Background);
         }
 
         Ok(res)
@@ -400,7 +405,7 @@ where
                     // An execute error here isn't important, we're just trying
                     // to prevent a waste of a socket...
                     #[cfg_attr(feature = "deprecated", allow(deprecated))]
-                    self.conn_builder.exec.execute(bg);
+                    self.conn_builder.exec.execute(bg, DeadlineHint::Background);
                 }
                 Ok(checked_out)
             }
@@ -510,6 +515,7 @@ where
                             executor.execute(
                                 conn.map_err(|e| debug!("client connection error: {}", e))
                                     .map(|_| ()),
+                                DeadlineHint::Background,
                             );
 
                             // Wait for 'conn' to ready up before we
