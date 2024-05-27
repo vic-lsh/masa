b24d3f6d1556cae1bfcb80ed05fd910b5fce1b84
Weixin Deng <weixin@cs.washington.edu>
2024-05-27 00:50:39 +0000

    bug: Need to add DeadlineHint in RawTask
    Runnable and RawTask are strongly coupled. We store DeadlineHint in Runnable, but no in RawTask.

--- examples/src/helloworld/client.rs
+++ examples/src/helloworld/client.rs
@@ -1,6 +1,7 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
+use structopt::StructOpt;
 
 use hello_world::greeter_client::GreeterClient;
 use hello_world::HelloRequest;
@@ -9,44 +10,48 @@ pub mod hello_world {
     tonic::include_proto!("helloworld");
 }
 
-async fn loadgen() -> Result<(), Box<dyn std::error::Error>> {
-    let client = GreeterClient::connect("http://[::1]:50051").await?;
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(about = "Client for benchmarking")]
+pub struct Args {
+    #[structopt(short, long, default_value = "http://[::1]:50051")]
+    pub addr: String,
+}
 
-    let counter = Arc::new(AtomicUsize::new(0));
-    let c = counter.clone();
+async fn loadgen(addr: String) -> Result<(), Box<dyn std::error::Error>> {
+    let cnt = Arc::new(AtomicUsize::new(0));
+    let c = cnt.clone();
     tokio::spawn(async move {
         let mut prev = 0;
         loop {
             tokio::time::sleep(Duration::from_secs(1)).await;
-            let c_curr = c.load(Ordering::Relaxed);
-            let rps = c_curr - prev;
+            let now = c.load(Ordering::Relaxed);
+            let rps = now - prev;
             println!("rps: {}", rps);
-            prev = c_curr;
+            prev = now;
         }
     });
 
-    let num_clients = 256; // tunable
+    let num_clients = 32;
     let mut handles = Vec::with_capacity(num_clients);
-    for _ in 0..num_clients {
-        let mut cl = client.clone();
-        let cnt = counter.clone();
 
+    let client = GreeterClient::connect(addr).await?;
+    for _ in 0..num_clients {
+        let mut client = client.clone();
+        let c = cnt.clone();
         let h = tokio::spawn(async move {
             let request = HelloRequest {
                 name: "Tonic".into(),
             };
             loop {
-                let _ = cl
+                let _ = client
                     .say_hello(tonic::Request::new(request.clone()))
                     .await
                     .unwrap();
-                cnt.fetch_add(1, Ordering::Relaxed);
+                c.fetch_add(1, Ordering::Relaxed);
             }
         });
-
         handles.push(h);
     }
-
     for h in handles {
         h.await.unwrap();
     }
@@ -56,5 +61,6 @@ async fn loadgen() -> Result<(), Box<dyn std::error::Error>> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    loadgen().await
+    let args = Args::from_args();
+    loadgen(args.addr).await
 }
