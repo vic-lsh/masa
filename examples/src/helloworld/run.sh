167cea1a2cc2bfe3995387e54a91d636f306405b
Weixin Deng <weixin@cs.washington.edu>
2024-05-28 16:45:50 -0700

    chore: Test client bench

--- examples/src/helloworld/run.sh
+++
@@ -1,8 +0,0 @@
-cargo run --release --bin helloworld-client-bench -- \
-	--rps 100 \
-	--secs 10 \
-	--concurrency 32 \
-	--addr1 http://[::1]:50051 \
-	--addr2 http://[::1]:50052 \
-	--output1 client1.txt \
-	--output2 client2.txt
