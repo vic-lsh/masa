29d871d8b5f28ad730f6dc4b1decef8377c779bb
vic-lsh <shli@cs.washington.edu>
2024-05-28 22:52:04 -0700

    fix: DeadlineHint import in examples

--- examples/src/helloworld/server.rs
+++ examples/src/helloworld/server.rs
@@ -3,13 +3,14 @@ use futures_lite::future;
 use hello_world::greeter_client::GreeterClient;
 use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
-use hyper::rt::{DeadlineHint, Exec, Executor};
+use hyper::rt::{Exec, Executor};
 use rand_distr::{Distribution, Normal};
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 use std::time::{SystemTime, UNIX_EPOCH};
 use structopt::StructOpt;
 use tonic::{transport::Server, Request, Response, Status};
+use tonic_deadline::DeadlineHint;
 
 pub mod hello_world {
     tonic::include_proto!("helloworld");
