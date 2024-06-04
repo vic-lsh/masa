3a524cc36f422d8be0b92bce9c213f1ae1077a75
Weixin Deng <weixin@cs.washington.edu>
2024-06-04 15:04:33 -0700

    feat: Add raw data

--- reboot/snippets/poc/ex1/run.sh
+++ reboot/snippets/poc/ex1/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rps_values=(50 100 150 200 250)
+rps_values=(25 50 75 100 125 150 175 200 225 250)
 
 for rps in "${rps_values[@]}"; do
   echo "Running benchmark for RPS: $rps..."
 
   cargo run --release --bin charleston_client_bench -- \
     --rps "$rps" \
-    --secs 60 \
+    --secs 300 \
     --concurrency 32 \
     --addr1 http://[::1]:50051 \
     --addr2 http://[::1]:50052 \
