52e9e9420e98964c3624dfdccee603292b27e170
Weixin Deng <weixin@cs.washington.edu>
2024-06-03 23:24:08 -0700

    feat: Add poc snippets

---
+++ reboot/snippets/poc/ex1/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rps_values=(50 100 150 200 250)
+
+for rps in "${rps_values[@]}"; do
+  echo "Running benchmark for RPS: $rps..."
+
+  cargo run --release --bin charleston_client_bench -- \
+    --rps "$rps" \
+    --secs 60 \
+    --concurrency 32 \
+    --addr1 http://[::1]:50051 \
+    --addr2 http://[::1]:50052 \
+    --output1 "snippets/poc/ex1/r${rps}-client1.csv" \
+    --output2 "snippets/poc/ex1/r${rps}-client2.csv"
+
+  sleep 3
+done
+
+echo "All benchmarks completed"
