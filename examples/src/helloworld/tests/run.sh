1ebecc25af91a8a37669d244d268d5dfc228c999
Weixin Deng <weixin@cs.washington.edu>
2024-06-07 15:50:44 -0700

    Revert "chore: Clean up"
    This reverts commit 61ad13c9da9b3b66bc9cf373a2a97ebd09cf56a5.

---
+++ examples/src/helloworld/tests/run.sh
@@ -0,0 +1,11 @@
+cargo run --release --bin helloworld-client-bench -- \
+  --rps 100 \
+  --secs 10 \
+  --concurrency 32 \
+  --addr1 http://[::1]:50051 \
+  --addr2 http://[::1]:50052 \
+  --output1 client1.csv \
+  --output2 client2.csv
+
+cargo run --release --bin helloworld-server -- \
+  --num-threads 5
