2a29928cb0c3776e5224627f270a9a54056a4f68
Weixin Deng <weixin@cs.washington.edu>
2024-06-07 15:51:51 -0700

    Revert "chore: Add notes"
    This reverts commit fb06df682edec7dcf62bef97f8013819f39c391d.

--- 3rd_party/tower/tower-service/src/lib.rs
+++ 3rd_party/tower/tower-service/src/lib.rs
@@ -351,7 +351,7 @@ pub trait Service<Request> {
     ///
     /// Implementations are permitted to panic if `call` is invoked without
     /// obtaining `Poll::Ready(Ok(()))` from `poll_ready`.
-    // NOTE: change this to return Future and DDL.
+    // [NOTE] change this to return Future and DDL.
     fn call(&mut self, req: Request) -> Self::Future;
 }
 
