5100f4faa35d53869220ba5346d9d223cf4b2364
Weixin Deng <weixin@cs.washington.edu>
2024-06-19 15:50:08 -0700

    feat: Tail diff is proptional to sigma

---
+++ reboot/snippets/hotnets/run.sh
@@ -0,0 +1,13 @@
+cargo run --release --bin charleston_rivas -- \
+  --mode fcfs \
+  --rps 450 \
+  --secs 60 \
+  --concurrency 32 \
+  --output snippets/hotnets/fcfs.csv
+
+cargo run --release --bin charleston_rivas -- \
+  --mode masa \
+  --rps 450 \
+  --secs 60 \
+  --concurrency 32 \
+  --output snippets/hotnets/masa.csv
