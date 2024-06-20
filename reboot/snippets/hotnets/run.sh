75e12f3aad4b5c37def218c92479bc529f07d4e8
Weixin Deng <weixin@cs.washington.edu>
2024-06-19 20:41:56 -0700

    chore: Add brother server

--- reboot/snippets/hotnets/run.sh
+++ reboot/snippets/hotnets/run.sh
@@ -1,13 +1,27 @@
-cargo run --release --bin charleston_rivas -- \
+cargo run --release --bin charleston_lactose -- \
   --mode fcfs \
   --rps 450 \
   --secs 60 \
   --concurrency 32 \
   --output snippets/hotnets/fcfs.csv
 
-cargo run --release --bin charleston_rivas -- \
+cargo run --release --bin charleston_lactose -- \
   --mode masa \
   --rps 450 \
   --secs 60 \
   --concurrency 32 \
   --output snippets/hotnets/masa.csv
+
+# cargo run --release --bin charleston_rivas -- \
+#   --mode fcfs \
+#   --rps 450 \
+#   --secs 60 \
+#   --concurrency 32 \
+#   --output snippets/hotnets/fcfs.csv
+
+# cargo run --release --bin charleston_rivas -- \
+#   --mode masa \
+#   --rps 450 \
+#   --secs 60 \
+#   --concurrency 32 \
+#   --output snippets/hotnets/masa.csv
