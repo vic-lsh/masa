ad48f4f25778f85478983da872b35f8a9acb2db5
Weixin Deng <weixin@cs.washington.edu>
2024-06-19 21:04:07 -0700

    feat: Run with replicas

--- reboot/snippets/hotnets/run.sh
+++ reboot/snippets/hotnets/run.sh
@@ -1,13 +1,13 @@
 cargo run --release --bin charleston_lactose -- \
   --mode fcfs \
-  --rps 450 \
+  --rps 1500 \
   --secs 60 \
   --concurrency 32 \
   --output snippets/hotnets/fcfs.csv
 
 cargo run --release --bin charleston_lactose -- \
   --mode masa \
-  --rps 450 \
+  --rps 1500 \
   --secs 60 \
   --concurrency 32 \
   --output snippets/hotnets/masa.csv
