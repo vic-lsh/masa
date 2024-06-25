6bd16add310698604f6ef19e30c00f087d3efb03
Weixin Deng <weixin@cs.washington.edu>
2024-06-24 22:39:00 -0700

    feat: Add provision numbers for ten seqs

--- reboot/snippets/hotnets/gamma-1/run.sh
+++ reboot/snippets/hotnets/gamma-1/run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# rps_values=(100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000)
+rps_values=(100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000)
 
-rps_values=(200 400 600 800 1000 1200 1400 1600 1800 2000)
+# rps_values=(200 400 600 800 1000 1200 1400 1600 1800 2000)
 
 for rps in "${rps_values[@]}"; do
   echo "Running benchmark for RPS: $rps..."
 
   cargo run --release --bin charleston_lactose -- \
-    --depth 6 \
-    --exec-ks 1 1 1 1 1 1 \
-    --first-exec-mus 3000 3000 3000 3000 3000 3000 \
-    --second-exec-mus 3000 3000 3000 3000 3000 3000 \
-    --replicas 6 \
+    --depth 10 \
+    --exec-ks 1 1 1 1 1 1 1 1 1 1 \
+    --first-exec-mus 2000 2000 2000 2000 2000 2000 2000 2000 2000 2000 \
+    --second-exec-mus 2000 2000 2000 2000 2000 2000 2000 2000 2000 2000 \
+    --replicas 4 \
     --mode fcfs \
     --rps "$rps" \
     --secs 60 \
@@ -22,11 +22,11 @@ for rps in "${rps_values[@]}"; do
   sleep 3
 
   cargo run --release --bin charleston_lactose -- \
-    --depth 6 \
-    --exec-ks 1 1 1 1 1 1 \
-    --first-exec-mus 3000 3000 3000 3000 3000 3000 \
-    --second-exec-mus 3000 3000 3000 3000 3000 3000 \
-    --replicas 6 \
+    --depth 10 \
+    --exec-ks 1 1 1 1 1 1 1 1 1 1 \
+    --first-exec-mus 2000 2000 2000 2000 2000 2000 2000 2000 2000 2000 \
+    --second-exec-mus 2000 2000 2000 2000 2000 2000 2000 2000 2000 2000 \
+    --replicas 4 \
     --mode masa \
     --rps "$rps" \
     --secs 60 \
