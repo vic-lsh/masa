3891dc16c08a89fdc4986f3c08aa86eadab837c4
Weixin Deng <weixin@cs.washington.edu>
2024-06-24 21:23:01 -0700

    feat: Add provision numbers

--- reboot/snippets/hotnets/run.sh
+++ reboot/snippets/hotnets/run.sh
@@ -1,8 +1,8 @@
 cargo run --release --bin charleston_lactose -- \
   --depth 6 \
-  --exec-ks 0.5 0.5 0.5 0.5 0.5 0.5 \
-  --first-exec-mus 1500 2000 2500 3500 4000 4500 \
-  --second-exec-mus 4500 4000 3500 2500 2000 1500 \
+  --exec-ks 1 1 1 1 1 1 \
+  --first-exec-mus 3000 3000 3000 3000 3000 3000 \
+  --second-exec-mus 3000 3000 3000 3000 3000 3000 \
   --replicas 6 \
   --mode fcfs \
   --rps 1600 \
@@ -12,9 +12,9 @@ cargo run --release --bin charleston_lactose -- \
 
 cargo run --release --bin charleston_lactose -- \
   --depth 6 \
-  --exec-ks 0.5 0.5 0.5 0.5 0.5 0.5 \
-  --first-exec-mus 1500 2000 2500 3500 4000 4500 \
-  --second-exec-mus 4500 4000 3500 2500 2000 1500 \
+  --exec-ks 1 1 1 1 1 1 \
+  --first-exec-mus 3000 3000 3000 3000 3000 3000 \
+  --second-exec-mus 3000 3000 3000 3000 3000 3000 \
   --replicas 6 \
   --mode masa \
   --rps 1600 \
