20116443b0fc30740a08e670fbb9e6ce5541e7f4
vic-lsh <shli@cs.washington.edu>
2024-07-18 22:10:50 -0700

    add DDLService trait

--- 3rd_party/tower/tower-service/src/lib.rs
+++ 3rd_party/tower/tower-service/src/lib.rs
@@ -5,6 +5,7 @@
     unreachable_pub
 )]
 #![forbid(unsafe_code)]
+#![feature(negative_impls)]
 // `rustdoc::broken_intra_doc_links` is checked on CI
 
 //! Definition of the core `Service` trait to Tower
@@ -388,3 +389,103 @@ where
         (**self).call(request)
     }
 }
+
+// pub trait DDLService<Request>: Service<Request> {
+pub trait DDLService<Request> {
+    /// Responses given by the service.
+    type Response;
+
+    /// Errors produced by the service.
+    type Error;
+
+    /// The future response value.
+    type Future: Future<Output = Result<Self::Response, Self::Error>>;
+
+    /// Returns `Poll::Ready(Ok(()))` when the service is able to process requests.
+    ///
+    /// If the service is at capacity, then `Poll::Pending` is returned and the task
+    /// is notified when the service becomes ready again. This function is
+    /// expected to be called while on a task. Generally, this can be done with
+    /// a simple `futures::future::poll_fn` call.
+    ///
+    /// If `Poll::Ready(Err(_))` is returned, the service is no longer able to service requests
+    /// and the caller should discard the service instance.
+    ///
+    /// Once `poll_ready` returns `Poll::Ready(Ok(()))`, a request may be dispatched to the
+    /// service using `call`. Until a request is dispatched, repeated calls to
+    /// `poll_ready` must return either `Poll::Ready(Ok(()))` or `Poll::Ready(Err(_))`.
+    ///
+    /// Note that `poll_ready` may reserve shared resources that are consumed in a subsequent
+    /// invocation of `call`. Thus, it is critical for implementations to not assume that `call`
+    /// will always be invoked and to ensure that such resources are released if the service is
+    /// dropped before `call` is invoked or the future returned by `call` is dropped before it
+    /// is polled.
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>>;
+
+    /// Process the request and return the response asynchronously.
+    ///
+    /// This function is expected to be callable off task. As such,
+    /// implementations should take care to not call `poll_ready`.
+    ///
+    /// Before dispatching a request, `poll_ready` must be called and return
+    /// `Poll::Ready(Ok(()))`.
+    ///
+    /// # Panics
+    ///
+    /// Implementations are permitted to panic if `call` is invoked without
+    /// obtaining `Poll::Ready(Ok(()))` from `poll_ready`.
+    // [TODO] Return Future and DDL.
+    fn call(&mut self, req: Request) -> Self::Future;
+}
+
+// impl<Req, S> Service<Req> for S
+// where
+//     S: DDLService<Req> + NoRef,
+// {
+//     type Response = S::Response;
+//     type Error = S::Error;
+//     type Future = S::Future;
+//
+//     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+//         self.poll_ready(cx)
+//     }
+//
+//     fn call(&mut self, req: Req) -> Self::Future {
+//         // TODO: in the future, ignore the returned ddl
+//         self.call(req)
+//     }
+// }
+//
+// impl<'a, S, Request> DDLService<Request> for &'a mut S
+// where
+//     S: DDLService<Request> + 'a,
+// {
+//     type Response = S::Response;
+//     type Error = S::Error;
+//     type Future = S::Future;
+//
+//     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), S::Error>> {
+//         (**self).poll_ready(cx)
+//     }
+//
+//     fn call(&mut self, request: Request) -> S::Future {
+//         (**self).call(request)
+//     }
+// }
+//
+// impl<S, Request> DDLService<Request> for Box<S>
+// where
+//     S: DDLService<Request> + ?Sized,
+// {
+//     type Response = S::Response;
+//     type Error = S::Error;
+//     type Future = S::Future;
+//
+//     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), S::Error>> {
+//         (**self).poll_ready(cx)
+//     }
+//
+//     fn call(&mut self, request: Request) -> S::Future {
+//         (**self).call(request)
+//     }
+// }
