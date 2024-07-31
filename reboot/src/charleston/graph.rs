ee632d8f35ad41f056eec2238d4f826907009e3f
Weixin Deng <weixin@cs.washington.edu>
2024-07-31 11:11:37 -0700

    chore: Add distribution

--- reboot/src/charleston/graph.rs
+++ reboot/src/charleston/graph.rs
@@ -1,26 +1,32 @@
 use std::collections::HashMap;
-use tonic::metadata::{GlobalGraph, LocalGraph, Path, Span};
+use tonic::metadata::{Distribution, GlobalGraph, LocalGraph, Path, Span};
 
 pub fn get_global_graph() -> GlobalGraph {
     let local_graphs = {
         let mut graphs = HashMap::new();
         graphs.insert(
             "Source".to_string() as Path,
-            LocalGraph::new(vec![Span::new("/hello.Greeter/SayHello".to_string(), 1, 1)]),
+            LocalGraph::new(vec![Span::new(
+                "/hello.Greeter/SayHello".to_string(),
+                Distribution::new(1_000),
+            )]),
         );
         graphs.insert(
             "/hello.Greeter/SayHello".to_string() as Path,
             LocalGraph::new(vec![
-                Span::new("Head".to_string(), 2, 2),
-                Span::new("/hello.Greeter/SayGoodbye".to_string(), 3, 3),
-                Span::new("Tail".to_string(), 4, 4),
+                Span::new("Head".to_string(), Distribution::new(2)),
+                Span::new(
+                    "/hello.Greeter/SayGoodbye".to_string(),
+                    Distribution::new(3_000),
+                ),
+                Span::new("Tail".to_string(), Distribution::new(4)),
             ]),
         );
         graphs.insert(
             "/hello.Greeter/SayGoodbye".to_string() as Path,
             LocalGraph::new(vec![
-                Span::new("Head".to_string(), 5, 5),
-                Span::new("Tail".to_string(), 6, 6),
+                Span::new("Head".to_string(), Distribution::new(5)),
+                Span::new("Tail".to_string(), Distribution::new(6)),
             ]),
         );
         graphs
