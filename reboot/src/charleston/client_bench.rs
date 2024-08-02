4409292349abf33770fa3e5d184c530b2b480601
Weixin Deng <weixin@cs.washington.edu>
2024-08-02 15:27:03 -0700

    chore: Adjust client bench

--- reboot/src/charleston/client_bench.rs
+++ reboot/src/charleston/client_bench.rs
@@ -1,17 +1,27 @@
-use crossbeam_channel::{bounded, unbounded, Receiver, Sender, TryRecvError};
-use hello_world::greeter_client::GreeterClient;
-use hello_world::HelloRequest;
-use rand_distr::{Distribution, Exp};
+use crossbeam_channel::{unbounded, Receiver, Sender};
+use env_logger::{Builder, Env};
+use hello_world::{greeter_client::GreeterClient, HelloRequest};
+use log::info;
+use rand::{rngs::StdRng, SeedableRng};
+use rand_distr::{Distribution, Exp, Uniform};
 use std::fs::{self, File};
 use std::io::Write;
 use std::path::Path;
+use std::sync::atomic::AtomicUsize;
+use std::sync::{
+    atomic::{AtomicI32, Ordering},
+    Arc,
+};
 use std::time::{SystemTime, UNIX_EPOCH};
 use structopt::StructOpt;
 use tokio::time::{Duration, Instant};
+use tonic::metadata::{Context, GlobalGraph};
+use tonic::transport::Channel;
 
 pub mod hello_world {
     tonic::include_proto!("hello");
 }
+mod graph;
 
 pub fn time_now() -> u64 {
     let now = SystemTime::now()
@@ -22,107 +32,31 @@ pub fn time_now() -> u64 {
 }
 
 #[derive(StructOpt, Debug, Clone)]
-#[structopt(about = "Client for benchmarking")]
+#[structopt(about = "Reboot for simulation")]
 pub struct Args {
+    #[structopt(short, long, required = true)]
+    pub slo: u64,
     #[structopt(short, long, required = true)]
     pub rps: u64,
     #[structopt(short, long, required = true)]
     pub secs: u64,
     #[structopt(short, long, required = true)]
-    pub concurrency: u32,
-    #[structopt(short, long, default_value = "http://[::1]:50051")]
-    pub addr1: String,
-    #[structopt(short, long, default_value = "http://[::1]:50052")]
-    pub addr2: String,
+    pub concurrency: u64,
     #[structopt(short, long, required = true)]
-    pub output1: String,
-    #[structopt(short, long, required = true)]
-    pub output2: String,
+    pub output: String,
 }
 
 #[derive(Debug, Clone)]
-struct Span {
-    send_at: u64,
-    recv_at: u64,
+pub struct Span {
+    span: String,
+    slo: u64,
+    latency: u64,
 }
 
-async fn load_gen(
-    rps: u64,
-    secs: u64,
-    concurrency: u32,
-    addr: String,
-    trace_tx: Sender<Span>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let client = GreeterClient::connect(addr).await?;
-
-    let start_at = Instant::now();
-    let pause_at = start_at + Duration::from_secs(secs);
-
-    let mut elapse = 0f64;
-    let exponential = Exp::new(rps as f64).unwrap();
-
-    let (request_tx, request_rx) = bounded(concurrency as usize);
-
-    let mut handles = Vec::new();
-    let handle = tokio::spawn(async move {
-        let request = HelloRequest {
-            name: "Tonic".into(),
-        };
-        loop {
-            let now = Instant::now();
-            if now > pause_at {
-                break;
-            }
-            let send_at = start_at + Duration::from_secs_f64(elapse);
-            tokio::time::sleep_until(send_at).await;
-            let value = {
-                let mut rng = rand::thread_rng();
-                exponential.sample(&mut rng)
-            };
-            elapse += value;
-            let request = request.clone();
-            while request_tx.is_full() {
-                tokio::task::yield_now().await;
-            }
-            request_tx.send(request).unwrap();
-        }
-    });
-    handles.push(handle);
-
-    for _ in 0..concurrency {
-        let mut client = client.clone();
-        let request_rx = request_rx.clone();
-        let trace_tx = trace_tx.clone();
-        let handle = tokio::spawn(async move {
-            loop {
-                match request_rx.try_recv() {
-                    Ok(request) => {
-                        let send_at = time_now();
-                        let _ = client
-                            .say_hello(tonic::Request::new(request))
-                            .await
-                            .unwrap();
-                        let recv_at = time_now();
-                        let span = Span { send_at, recv_at };
-                        trace_tx.send(span).unwrap();
-                    }
-                    Err(TryRecvError::Disconnected) => {
-                        break;
-                    }
-                    Err(TryRecvError::Empty) => {
-                        tokio::task::yield_now().await;
-                    }
-                }
-            }
-        });
-        handles.push(handle);
+impl Span {
+    pub fn new(span: String, slo: u64, latency: u64) -> Self {
+        Self { span, slo, latency }
     }
-
-    for h in handles {
-        h.await.unwrap();
-    }
-
-    Ok(())
 }
 
 async fn fetch_traces(output: String, trace_rx: Receiver<Span>) {
@@ -133,41 +67,189 @@ async fn fetch_traces(output: String, trace_rx: Receiver<Span>) {
         }
     }
     let mut file = File::create(output).unwrap();
+    writeln!(file, "span,slo,latency").unwrap();
     while let Ok(span) = trace_rx.recv() {
-        let latency = span.recv_at - span.send_at;
-        writeln!(file, "{}", latency).unwrap();
+        writeln!(file, "{},{},{}", span.span, span.slo, span.latency).unwrap();
     }
 }
 
-// NOTE: This benchmark consists of two clients sending the same RPS to one server.
-// The server has one executor but assigns different deadlines to two request classes.
+#[derive(Debug)]
+struct LoadGenerator {
+    rng: StdRng,
+    slo: u64,
+    rps: u64,
+    secs: u64,
+    token: Arc<AtomicI32>,
+    global_graph: GlobalGraph,
+    client: GreeterClient<Channel>,
+    trace_tx: Sender<Span>,
+}
+
+impl LoadGenerator {
+    fn new(
+        rng: StdRng,
+        slo: u64,
+        rps: u64,
+        secs: u64,
+        token: Arc<AtomicI32>,
+        global_graph: GlobalGraph,
+        client: GreeterClient<Channel>,
+        trace_tx: Sender<Span>,
+    ) -> Self {
+        LoadGenerator {
+            rng,
+            slo,
+            rps,
+            secs,
+            token,
+            global_graph,
+            client,
+            trace_tx,
+        }
+    }
+
+    async fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let counter = Arc::new(AtomicUsize::new(0));
+        let counter_clone = counter.clone();
+        tokio::task::spawn(async move {
+            let mut counter_before = 0;
+            loop {
+                tokio::time::sleep(Duration::from_secs(1)).await;
+                let counter_now = counter_clone.load(Ordering::Relaxed);
+                info!("RPS: {}", counter_now - counter_before);
+                counter_before = counter_now;
+            }
+        });
+
+        let init_at = Instant::now();
+        let init_at_u64 = time_now();
+        let pause_at = init_at + Duration::from_secs(self.secs);
+
+        let mut elapse = 0f64;
+        let exponential = Exp::new(self.rps as f64).unwrap();
+        let uniform = Uniform::new(0, 1_000_000_007);
+
+        let graph_id = self.global_graph.graph_id().clone();
+        let local_graph = self.global_graph.get_source().clone();
+        let request = HelloRequest {
+            name: "Tonic".into(),
+        };
+
+        loop {
+            if Instant::now() > pause_at {
+                break;
+            }
+
+            if self.token.load(Ordering::SeqCst) <= 0 {
+                continue;
+            }
+
+            let start_at = init_at + Duration::from_secs_f64(elapse);
+            tokio::time::sleep_until(start_at).await;
+
+            let value = exponential.sample(&mut self.rng);
+            elapse += value;
+
+            let request = {
+                let request_id = uniform.sample(&mut self.rng);
+                let start_at = time_now() - init_at_u64;
+                let deadline = start_at + self.slo;
+                let ctx = Context::new(
+                    graph_id.clone(),
+                    request_id,
+                    start_at,
+                    deadline,
+                    Some(local_graph.clone()),
+                );
+                let mut request = tonic::Request::new(request.clone());
+                request.metadata_mut().insert_ctx("par_ctx", &ctx);
+                request
+            };
+
+            if self.token.load(Ordering::SeqCst) > 0 {
+                counter.fetch_add(1, Ordering::Relaxed);
+                let token = self.token.clone();
+                token.fetch_sub(1, Ordering::SeqCst);
+
+                let slo = self.slo;
+                let mut client = self.client.clone();
+                let trace_tx = self.trace_tx.clone();
+
+                tokio::task::spawn(async move {
+                    let send_at = time_now();
+                    client.say_hello(request).await.unwrap();
+                    token.fetch_add(1, Ordering::SeqCst);
+                    let recv_at = time_now();
+                    let latency = recv_at - send_at;
+                    let span = Span::new("SayHello".to_string(), slo, latency);
+                    trace_tx.try_send(span).unwrap();
+                });
+            }
+        }
+
+        info!("Load gen completed");
+        Ok(())
+    }
+}
+
+fn init_logging() {
+    Builder::from_env(Env::default().default_filter_or("info"))
+        .format(|buf, record| {
+            use std::io::Write;
+            writeln!(
+                buf,
+                "{} [{}:{}] {}",
+                record.level(),
+                record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
+                // record.target(),
+                record.args()
+            )
+        })
+        .init();
+    info!("Logging initialized");
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    init_logging();
+
     let args = Args::from_args();
-    eprintln!("args: {:?}", args);
 
     let (trace_tx, trace_rx) = unbounded();
-    let gen1 = tokio::spawn(async move {
-        load_gen(args.rps, args.secs, args.concurrency, args.addr1, trace_tx)
-            .await
-            .unwrap();
-    });
-    let trace1 = tokio::spawn(async move {
-        fetch_traces(args.output1, trace_rx).await;
-    });
 
-    let (trace_tx, trace_rx) = unbounded();
-    let gen2 = tokio::spawn(async move {
-        load_gen(args.rps, args.secs, args.concurrency, args.addr2, trace_tx)
-            .await
-            .unwrap();
-    });
-    let trace2 = tokio::spawn(async move {
-        fetch_traces(args.output2, trace_rx).await;
-    });
+    let mut handles = Vec::new();
 
-    let handles = vec![gen1, trace1, gen2, trace2];
+    let mut load_gen = {
+        const KEY: u64 = 13;
+        const SEED: u64 = 998244353;
+        let seed = SEED * KEY + args.rps;
+        let rng = StdRng::seed_from_u64(seed);
+        let token = Arc::new(AtomicI32::new(args.concurrency as i32));
+        let global_graph = graph::get_global_graph();
+        let client = GreeterClient::connect("http://[::1]:50051").await?;
+
+        let load_gen = LoadGenerator::new(
+            rng,
+            args.slo,
+            args.rps,
+            args.secs,
+            token,
+            global_graph,
+            client,
+            trace_tx,
+        );
+        load_gen
+    };
+
+    handles.push(tokio::spawn(async move {
+        load_gen.run().await.unwrap();
+    }));
+
+    handles.push(tokio::spawn(async move {
+        fetch_traces(args.output, trace_rx).await;
+    }));
+
     for h in handles {
         h.await.unwrap();
     }
