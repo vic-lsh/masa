2805d1e18cafe7c8d2257098c1603b7426939249
Weixin Deng <weixin@cs.washington.edu>
2024-08-02 15:27:03 -0700

    chore: Adjust client bench

--- tonic/src/metadata/masa.rs
+++ tonic/src/metadata/masa.rs
@@ -43,12 +43,13 @@ impl Distribution {
 
     /// Sample a latency.
     pub fn sample(&self, request_id: RequestID) -> Latency {
-        if let Some(percentile_latencies) = &self.percentile_latencies {
-            let index = request_id as usize % percentile_latencies.len();
-            percentile_latencies[index]
-        } else {
-            panic!("No percentile latencies");
-        }
+        self.mean
+        // if let Some(percentile_latencies) = &self.percentile_latencies {
+        //     let index = request_id as usize % percentile_latencies.len();
+        //     percentile_latencies[index]
+        // } else {
+        //     panic!("No percentile latencies");
+        // }
     }
 }
 
