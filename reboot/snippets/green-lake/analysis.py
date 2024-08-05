d28e7d5ce2454585a0b1ef3dff2a7e12b7d810f2
Weixin Deng <weixin@cs.washington.edu>
2024-08-05 13:25:20 -0700

    chore: First things first

--- reboot/snippets/green-lake/analysis.py
+++ reboot/snippets/green-lake/analysis.py
@@ -1,24 +1,22 @@
 import pandas as pd
 
-rps = [100, 200]
-modes = ["fifo", "masa"]
+# rps = [100, 200]
+# modes = ["masa", "fifo"]
+
+rps = [800, 900, 1000, 1100, 1200]
+modes = ["masa", "fifo"]
+
+print(rps)
 
 for mode in modes:
     print(f"Results for {mode}:")
     for r in rps:
-        print("rps:", r)
-
         file = f"r{r}-{mode}.csv"
         df = pd.read_csv(file)
-
-        # mean_value = df["latency"].mean()
-        # stdev_value = df["latency"].std()
-        # print(f"mean: {round(mean_value)}")
-        # print(f"std: {round(stdev_value)}")
 
         percentiles = [0.99]
         latencies = list(df["latency"].quantile(percentiles))
         for i, p in enumerate(percentiles):
-            print(f"{int(p*100)}%: {round(latencies[i])}")
+            print(round(latencies[i]))
 
     print()
