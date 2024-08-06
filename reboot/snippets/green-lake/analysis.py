8ae8fc02775a5ece3111c913a9305dff86fe3f80
Weixin Deng <weixin@cs.washington.edu>
2024-08-06 11:37:40 -0700

    feat: Add first plot

--- reboot/snippets/green-lake/analysis.py
+++ reboot/snippets/green-lake/analysis.py
@@ -1,19 +1,21 @@
+from typing import *
+
 import pandas as pd
 
-rps = [700, 750, 800, 850, 900]
+rps = [r for r in range(300, 901, 25)]
 modes = ["masa", "fifo"]
 
-print(rps)
+results: Dict[str, List[int]] = {"rps": rps, "masa": [], "fifo": [], "relative": []}
 
-for mode in modes:
-    print(f"Results for {mode}:")
-    for r in rps:
+for r in rps:
+    for mode in modes:
         file = f"r{r}-{mode}.csv"
         df = pd.read_csv(file)
 
-        percentiles = [0.99]
-        latencies = list(df["latency"].quantile(percentiles))
-        for i, p in enumerate(percentiles):
-            print(round(latencies[i]))
+        latencies = df["latency"].quantile(0.99)
+        results[mode].append(round(latencies))
 
-    print()
+    results["relative"].append(results["fifo"][-1] * 100 // results["masa"][-1])
+
+df = pd.DataFrame(results)
+df.to_csv("results.csv", index=False)
