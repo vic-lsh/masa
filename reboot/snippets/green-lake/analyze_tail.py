2bd72150bfa72915cbc4c1e6e910aec1b8ac4782
Weixin Deng <weixin@cs.washington.edu>
2024-08-11 21:01:41 -0700

    chore: Plot cdf

---
+++ reboot/snippets/green-lake/analyze_tail.py
@@ -0,0 +1,23 @@
+from typing import *
+
+import pandas as pd
+
+percentiles = [0.01, 0.25, 0.5, 0.75, 0.99]
+rps = [r for r in range(300, 901, 25)]
+modes = ["masa", "fifo"]
+
+for pctl in percentiles:
+    results: Dict[str, List[int]] = {"rps": rps, "masa": [], "fifo": [], "relative": []}
+
+    for r in rps:
+        for mode in modes:
+            file = f"r{r}-{mode}.csv"
+            df = pd.read_csv(file)
+
+            latencies = df["latency"].quantile(pctl)
+            results[mode].append(round(latencies))
+
+        results["relative"].append(results["fifo"][-1] * 100 // results["masa"][-1])
+
+    df = pd.DataFrame(results)
+    df.to_csv(f"fig_tail_{pctl}.csv", index=False)
