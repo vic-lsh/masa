206b8042bc3ca569906ff67310529cbbbb04766c
Weixin Deng <weixin@cs.washington.edu>
2024-08-11 21:27:52 -0700

    chore: Plot cdf for depth=2, mean=2, sum=4

---
+++ reboot/snippets/flat/plot_cdf.py
@@ -0,0 +1,36 @@
+from typing import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+rps = 1600
+modes = ["masa", "fifo"]
+
+results: Dict[str, List[int]] = {"masa": [], "fifo": []}
+
+for mode in modes:
+    file = f"r{rps}-{mode}.csv"
+    df = pd.read_csv(file)
+
+    df["latency"] /= 1_000
+    latencies = list(df["latency"])
+    results[mode] = latencies
+
+fig = plt.figure(figsize=(10, 6))
+
+for mode in modes:
+    latencies = results[mode]
+    latencies.sort()
+    cdf = np.arange(1, len(latencies) + 1) / len(latencies)
+
+    plt.plot(latencies, cdf, label=mode)
+
+# plt.tight_layout()
+plt.xlabel("Latency (ms)")
+plt.ylabel("CDF")
+plt.title("Latency CDF")
+plt.legend()
+plt.grid(True)
+plt.savefig("fig_cdf.png")
+plt.show()
