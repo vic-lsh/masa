cf37d644e02f7915b54a10c2a965ec8bfe4f5699
Weixin Deng <weixin@cs.washington.edu>
2024-08-12 18:01:23 -0700

    feat: Add pdf figures

--- reboot/snippets/flat/plot_e2e.py
+++ reboot/snippets/flat/plot_e2e.py
@@ -24,10 +24,8 @@ def plot_cdf(results: Dict[str, List[int]], title: str, fig_name: str):
         latencies = [r / 1e3 for r in results[mode]]
         latencies.sort()
         cdf = np.arange(1, len(latencies) + 1) / len(latencies)
-
         plt.plot(latencies, cdf, label=mode)
 
-    # plt.tight_layout()
     plt.xlabel("Latency (ms)")
     plt.ylabel("CDF")
     plt.title(title)
@@ -37,10 +35,27 @@ def plot_cdf(results: Dict[str, List[int]], title: str, fig_name: str):
     plt.show()
 
 
-plot_cdf(results, f"Latency CDF (RPS={rps})", f"fig_e2e_cdf.png")
+def plot_pdf(results: Dict[str, List[int]], title: str, fig_name: str):
+    fig = plt.figure(figsize=(10, 6))
+
+    for mode in modes:
+        latencies = [r / 1e3 for r in results[mode]]
+        plt.hist(latencies, bins=1000, density=True, histtype="step", label=mode)
+
+    plt.xlabel("Latency (ms)")
+    plt.ylabel("PDF")
+    plt.title(title)
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(fig_name)
+    plt.show()
 
 
-def plot_cdf_relative(results: List[float], title: str, fig_name: str):
+plot_cdf(results, f"Latency CDF (rps={rps})", f"fig_e2e_cdf.png")
+plot_pdf(results, f"Latency PDF (rps={rps})", f"fig_e2e_pdf.png")
+
+
+def plot_ratio_cdf(results: List[float], title: str, fig_name: str):
     fig = plt.figure(figsize=(10, 6))
 
     results.sort()
@@ -61,10 +76,9 @@ def plot_cdf_relative(results: List[float], title: str, fig_name: str):
                     label=f"CDF at x={poi}: {y_value:.2f}",
                 )
 
-    plt.legend()  # Show legend with the label
+    plt.legend()
 
-    # plt.tight_layout()
-    plt.xlabel("Relative Latency (%)")
+    plt.xlabel("Latency Ratio (%)")
     plt.ylabel("CDF")
     plt.title(title)
     plt.grid(True)
@@ -72,11 +86,33 @@ def plot_cdf_relative(results: List[float], title: str, fig_name: str):
     plt.show()
 
 
-results_relative: List[float] = []
+def plot_ratio_pdf(results: List[float], title: str, fig_name: str):
+    fig = plt.figure(figsize=(10, 6))
+
+    plt.hist(results, bins=1000, density=True, histtype="step", label="masa / fifo")
+
+    plt.legend()
+
+    plt.xlabel("Latency Ratio (%)")
+    plt.ylabel("PDF")
+    plt.title(title)
+    plt.grid(True)
+    plt.savefig(fig_name)
+    plt.show()
+
+
+results_ratio: List[float] = []
 for i in range(len(results["masa"])):
-    results_relative.append(results["fifo"][i] * 100 / results["masa"][i])
-plot_cdf_relative(
-    results_relative,
-    f"Relative Latency CDF (fifo / masa) (rps={rps})",
-    f"fig_e2e_relative_cdf.png",
+    results_ratio.append(results["masa"][i] * 100 / results["fifo"][i])
+
+plot_ratio_cdf(
+    results_ratio,
+    f"Latency Ratio CDF (masa / fifo) (rps={rps})",
+    f"fig_e2e_ratio_cdf.png",
+)
+
+plot_ratio_pdf(
+    results_ratio,
+    f"Latency Ratio PDF (masa / fifo) (rps={rps})",
+    f"fig_e2e_ratio_pdf.png",
 )
