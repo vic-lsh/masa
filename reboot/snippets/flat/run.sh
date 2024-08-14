a30ece89489ffe93156704bd9b231d1d4dece7d5
Weixin Deng <weixin@cs.washington.edu>
2024-08-14 11:55:11 -0700

    (WIP) chore: Add flat-test-eq-3-fail

--- reboot/snippets/flat/run.sh
+++ reboot/snippets/flat/run.sh
@@ -4,7 +4,7 @@ path="snippets/flat"
 
 # rps_values=(300 325 350 375 400 425 450 475 500 525 550 575 600 625 650 675 700 725 750 775 800 825 850 875 900)
 # rps_values=(200 1600)
-rps_values=(1600)
+rps_values=(800)
 
 modes=("masa" "fifo")
 
@@ -27,7 +27,7 @@ for mode in "${modes[@]}"; do
 		cargo run --release --bin bridgeway_client_bench -- \
 			--slo 10000 \
 			--rps $rps \
-			--secs 120 \
+			--secs 60 \
 			--concurrency 512 \
 			--output $path/r${rps}_${mode}.csv \
 			>/dev/null 2>&1
