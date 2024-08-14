8ebb541a0ca2fad4801c694028926f3d622297ea
Weixin Deng <weixin@cs.washington.edu>
2024-08-14 14:58:48 -0700

    (WIP) chore: Add flat-test-le-6

--- reboot/snippets/flat/plot_e2e.py
+++ reboot/snippets/flat/plot_e2e.py
@@ -1,108 +1,22 @@
 from typing import *
 
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-import numpy as np
 import pandas as pd
-
-plt.rcParams["font.family"] = "Roboto"
-fontsize = 17
-plt.rcParams.update(
-    {
-        "font.size": fontsize,
-        "axes.labelsize": fontsize,
-        "axes.titlesize": fontsize,
-        "xtick.labelsize": fontsize,
-        "ytick.labelsize": fontsize,
-        "legend.fontsize": fontsize,
-    }
+from plot_core import (  # type: ignore
+    MODES,
+    plot_2d_histogram,
+    plot_cdf,
+    plot_pdf,
+    plot_ratio_cdf,
+    plot_ratio_pdf,
 )
 
-rps = 1600
-modes = ["masa", "fifo"]
+rps = 400
 
 results_e2e: Dict[str, List[int]] = {}
-for mode in modes:
+for mode in MODES:
     file = f"r{rps}_{mode}_filtered.csv"
     df = pd.read_csv(file)
     results_e2e[mode] = list(df["latency"])
-
-
-def plot_cdf(results: Dict[str, List[int]], title: str, fig_name: str):
-    fig = plt.figure(figsize=(10, 6))
-
-    for mode in modes:
-        latencies = [r / 1e3 for r in results[mode]]
-        latencies.sort()
-        cdf = np.arange(1, len(latencies) + 1) / len(latencies)
-        plt.plot(latencies, cdf, label=mode)
-
-    plt.xlabel("Latency (ms)")
-    plt.ylabel("CDF")
-    plt.title(title)
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(fig_name)
-    plt.show()
-
-
-def plot_pdf(results: Dict[str, List[int]], title: str, fig_name: str):
-    fig = plt.figure(figsize=(10, 6))
-
-    for mode in modes:
-        latencies = [r / 1e3 for r in results[mode]]
-
-        counts, bin_edges = np.histogram(latencies, bins=1000, density=True)
-        bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
-        bin_width = bin_edges[1] - bin_edges[0]
-        # Calculate expected value E[X] = sum(x * p(x) * width of bin)
-        expected_value = np.sum(bin_centers * counts * bin_width)
-
-        plt.hist(
-            latencies,
-            bins=1000,
-            density=True,
-            histtype="step",
-            label=f"{mode} (E[X]={expected_value:.2f}ms)",
-        )
-
-    plt.xlabel("Latency (ms)")
-    plt.ylabel("PDF")
-    plt.title(title)
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(fig_name)
-    plt.show()
-
-
-def plot_2d_histogram(results: Dict[str, List[int]], title, fig_name):
-    fig, ax = plt.subplots(figsize=(10, 6))
-
-    masa_latencies = [r / 1e3 for r in results["masa"]]
-    fifo_latencies = [r / 1e3 for r in results["fifo"]]
-    h, xedges, yedges, img = ax.hist2d(
-        masa_latencies,
-        fifo_latencies,
-        range=[[0, 30], [0, 30]],
-        bins=100,
-        cmap="inferno",
-        density=True,
-    )
-    ax.set_xlabel("masa Latency (ms)")
-    ax.set_ylabel("fifo Latency (ms)")
-    ax.set_title(title)
-
-    ax.set_aspect("equal")
-    ax.xaxis.set_major_locator(ticker.LinearLocator(7))
-    ax.yaxis.set_major_locator(ticker.LinearLocator(7))
-
-    cbar = fig.colorbar(img, ax=ax)
-    cbar.ax.set_ylabel("Density")
-
-    plt.grid(True)
-    plt.savefig(fig_name)
-    plt.show()
-
 
 plot_cdf(results_e2e, f"E2E Latency CDF (rps={rps})", f"fig_e2e_cdf.png")
 
@@ -115,52 +29,6 @@ plot_2d_histogram(
 )
 
 exit()
-
-
-def plot_ratio_cdf(results: List[float], title: str, fig_name: str):
-    fig = plt.figure(figsize=(10, 6))
-
-    results.sort()
-    cdf = np.arange(1, len(results) + 1) / len(results)
-
-    plt.plot(results, cdf)
-
-    pois = [100, 200, 300, 400, 500]
-    for poi in pois:
-        # plt.axvline(x=poi, color="r", linestyle="--")
-        if results[-1] >= poi:
-            y_value = next((y for x, y in zip(results, cdf) if x >= poi), None)
-            if y_value is not None:
-                plt.axhline(
-                    y=y_value,
-                    color="forestgreen",
-                    linestyle="--",
-                    label=f"CDF at x={poi}: {y_value:.2f}",
-                )
-
-    plt.legend()
-
-    plt.xlabel("Latency Ratio (%)")
-    plt.ylabel("CDF")
-    plt.title(title)
-    plt.grid(True)
-    plt.savefig(fig_name)
-    plt.show()
-
-
-def plot_ratio_pdf(results: List[float], title: str, fig_name: str):
-    fig = plt.figure(figsize=(10, 6))
-
-    plt.hist(results, bins=1000, density=True, histtype="step", label="masa / fifo")
-
-    plt.legend()
-
-    plt.xlabel("Latency Ratio (%)")
-    plt.ylabel("PDF")
-    plt.title(title)
-    plt.grid(True)
-    plt.savefig(fig_name)
-    plt.show()
 
 
 results_ratio: List[float] = []
