eafa99ed7516946e189852b1db1a481cf4956c1a
Weixin Deng <weixin@cs.washington.edu>
2024-09-03 13:36:04 -0700

    chore: Reorg common code

---
+++ reboot/src/bridgeway/exec.rs
@@ -0,0 +1,36 @@
+use futures_lite::future;
+use hyper::rt::Executor;
+use std::sync::Arc;
+use tonic_masa::DeadlineHint;
+
+#[derive(Debug)]
+pub struct ExecImpl<'a> {
+    ex: Arc<smol::Executor<'a>>,
+}
+
+impl<'a> ExecImpl<'a> {
+    pub fn new(ex: Arc<smol::Executor<'a>>) -> Self {
+        Self { ex }
+    }
+
+    pub async fn run(&self) {
+        // [NOTE] Only a global queue is used in smol::Executor::tick().
+        loop {
+            self.ex.tick().await;
+            // [TODO] Change to tokio yield.
+            // [NOTE] Yield to tokio runtime.
+            future::yield_now().await;
+        }
+    }
+}
+
+impl<'a, F> Executor<F> for ExecImpl<'a>
+where
+    F: std::future::Future + Send + 'static,
+    F::Output: Send,
+{
+    fn execute(&self, fut: F, ddl: DeadlineHint) {
+        // [NOTE] Deadline is passed from H2Stream.
+        self.ex.spawn_with_ddl(fut, ddl).fallible().detach();
+    }
+}
