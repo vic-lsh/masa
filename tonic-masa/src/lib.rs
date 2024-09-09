680155c9b008a21adfbe462a64e9b993e203a185
Weixin Deng <weixin@cs.washington.edu>
2024-09-08 19:48:54 -0700

    chore: Reorg flags to tonic-masa
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- tonic-masa/src/lib.rs
+++ tonic-masa/src/lib.rs
@@ -1,8 +1,31 @@
 mod context;
 mod deadline;
+mod tracker;
 
 pub use context::{
     Address, Context, Distribution, GlobalGraph, GraphID, Latency, LocalGraph, Path, RequestID,
     Span, Timestamp,
 };
 pub use deadline::DeadlineHint;
+pub use tracker::LatencyTracker;
+
+pub const EST_OFFLINE: bool = if cfg!(feature = "est_offline") {
+    true
+} else {
+    false
+};
+pub const EST_ONLINE: bool = if cfg!(feature = "est_online") {
+    true
+} else {
+    false
+};
+pub const MOCK_DIST: bool = if cfg!(feature = "mock_dist") {
+    true
+} else {
+    false
+};
+pub const MOCK_HOTEL: bool = if cfg!(feature = "mock_hotel") {
+    true
+} else {
+    false
+};
