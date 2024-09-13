e3d828113a079c17135b2a6b5d07f3089476d3ec
vic-lsh <shli@cs.washington.edu>
2024-09-13 12:27:40 -0700

    feat(tonic-masa): add various ctx structs for use in tonic

--- tonic-masa/src/lib.rs
+++ tonic-masa/src/lib.rs
@@ -3,6 +3,6 @@ mod deadline;
 
 pub use context::{
     Address, Context, Distribution, GlobalGraph, GraphID, Latency, LocalGraph, Path, RequestID,
-    Span, Timestamp,
+    RequestRxContext, RequestTxContext, ServerContext, Span, Timestamp,
 };
 pub use deadline::DeadlineHint;
