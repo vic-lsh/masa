428ab691508e63742eaaf6501c299f9e2b282c53
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-09-14 16:04:47 -0700

    replace id in message with random number

--- reboot/src/socialNetwork/uniqueIdService/src/client.rs
+++ reboot/src/socialNetwork/uniqueIdService/src/client.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use unique_id_service::greeter_client::GreeterClient;
 use unique_id_service::UniqueIdRequest;
 
@@ -9,7 +10,9 @@ pub mod unique_id_service {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = GreeterClient::connect("http://[::1]:50051").await?;
 
-    let request: tonic::Request<UniqueIdRequest> = tonic::Request::new(UniqueIdRequest { id: 1 });
+    let request: tonic::Request<UniqueIdRequest> = tonic::Request::new(UniqueIdRequest {
+        id: rand::thread_rng().gen_range(1..=100),
+    });
     let response = client.compose_unique_id(request).await?;
 
     println!("RESPONSE={:?}", response);
