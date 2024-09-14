5cd22ca9834149a1e90c25e7e5b64fdd6bc30b49
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-09-14 15:46:27 -0700

    add ComposeUniqueId rpc

---
+++ reboot/src/socialNetwork/uniqueIdService/src/client.rs
@@ -0,0 +1,18 @@
+use unique_id_service::greeter_client::GreeterClient;
+use unique_id_service::UniqueIdRequest;
+
+pub mod unique_id_service {
+    tonic::include_proto!("unique");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    let request: tonic::Request<UniqueIdRequest> = tonic::Request::new(UniqueIdRequest { id: 1 });
+    let response = client.compose_unique_id(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
---
+++ reboot/src/socialNetwork/uniqueIdService/src/server.rs
@@ -0,0 +1,39 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use unique_id_service::greeter_server::{Greeter, GreeterServer};
+use unique_id_service::{UniqueIdReply, UniqueIdRequest};
+
+pub mod unique_id_service {
+    tonic::include_proto!("unique");
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn compose_unique_id(
+        &self,
+        request: Request<UniqueIdRequest>,
+    ) -> Result<Response<UniqueIdReply>, Status> {
+        println!("Got a request: {:?}", request);
+        let reply = UniqueIdReply {
+            message: request.into_inner().id,
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
