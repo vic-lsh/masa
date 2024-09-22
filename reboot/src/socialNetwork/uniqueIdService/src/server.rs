e93f89663dd597ec6d17a7f5d28e46cd826c53b6
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-09-22 13:22:28 -0700

    finish hash_mac_address_pid

--- reboot/src/socialNetwork/uniqueIdService/src/server.rs
+++ reboot/src/socialNetwork/uniqueIdService/src/server.rs
@@ -1,3 +1,6 @@
+use std::process;
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use tonic::{transport::Server, Request, Response, Status};
 
 use unique_id_service::greeter_server::{Greeter, GreeterServer};
@@ -8,7 +11,9 @@ pub mod unique_id_service {
 }
 
 #[derive(Debug, Default)]
-pub struct MyGreeter {}
+pub struct MyGreeter {
+    machine_id: String,
+}
 
 #[tonic::async_trait]
 impl Greeter for MyGreeter {
@@ -20,15 +25,50 @@ impl Greeter for MyGreeter {
         let reply = UniqueIdReply {
             message: request.into_inner().id,
         };
+        // Initialize the random number generator
+        let _machine_id = self.machine_id.clone();
+
+        // Get the current system time
+        let start = SystemTime::now();
+
+        // Calculate the duration since the UNIX epoch
+        let duration = start
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
+
+        // Get the timestamp in milliseconds
+        let timestamp: i64 = duration.as_millis() as i64;
+
+        let counter_hex = "1";
+
+        // Generate a random ID between 1 and 100 (inclusive)
+        let random_id = format!("{}{}{}", _machine_id, timestamp_hex, counter_hex);
 
         Ok(Response::new(reply))
     }
 }
 
+fn get_machine_id(netif: &str) -> String {
+    todo!()
+}
+
+fn hash_mac_address_pid(mac: &str) -> u16 {
+    let mut hash: u16 = 0;
+    let pid = process::id().to_string();
+    let mac_pid = format!("{}{}", mac, pid);
+    for (i, byte) in mac_pid.bytes().enumerate() {
+        hash += (byte as u16) << ((i & 1) * 8);
+    }
+    hash
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let greeter = MyGreeter::default();
+    let netif = "";
+    let greeter = MyGreeter {
+        machine_id: get_machine_id(netif),
+    };
 
     Server::builder()
         .add_service(GreeterServer::new(greeter))
