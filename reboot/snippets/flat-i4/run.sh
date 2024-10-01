d7193ae7ad044795403ec983984c336f87d8c849
Weixin Deng <weixin@cs.washington.edu>
2024-10-01 16:46:34 -0700

    test: Add test case for i4
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- reboot/snippets/flat-i4/run.sh
+++ reboot/snippets/flat-i4/run.sh
@@ -2,9 +2,11 @@
 
 path="snippets/flat-i4"
 
-rps_values=(50)
+# rps_values=(300 325 350 375 400 425 450)
+rps_values=(800)
 
-modes=("queue_edf" "queue_fifo_two" "queue_fifo")
+# modes=("prio_local" "fifo_two" "fifo")
+modes=("prio_local" "fifo_two")
 
 server_pid=
 
@@ -25,13 +27,19 @@ for mode in "${modes[@]}"; do
 	echo "Compiling mode: $mode..."
 
 	# cargo build --features $mode >/dev/null 2>&1
-	cargo build --features $mode \
-		--release >/dev/null 2>&1
+	cargo build \
+		--release \
+		--features "$mode" \
+		>/dev/null 2>&1
 
-	# RUST_BACKTRACE=1 RUST_LOG=info cargo run ...
+	# RUST_BACKTRACE=1 RUST_LOG=info \
 	RUST_LOG=warn \
-		cargo run --features $mode \
-		--release --bin bridgeway_server -- \
+		cargo run \
+		--release \
+		--features "$mode" \
+		--bin bridgeway_server -- \
+		--graph-ids I4 \
+		--slos 20000 \
 		--n-hops 4 \
 		--n-threads 1 \
 		>$path/tmp_server_${mode}.log 2>&1 &
@@ -41,17 +49,20 @@ for mode in "${modes[@]}"; do
 	echo "Running server in mode: $mode..."
 
 	for rps in "${rps_values[@]}"; do
-		sleep 3
+		sleep 1
 
 		echo "Running benchmark for RPS: $rps..."
 
-		cargo run --release --bin bridgeway_client_bench -- \
-			--slo 10000 \
+		cargo run \
+			--release \
+			--features "$mode" \
+			--bin bridgeway_client_bench -- \
+			--graph-ids I4 \
+			--slos 20000 \
 			--rps $rps \
 			--secs 10 \
 			--concurrency 512 \
 			--output $path/r${rps}_${mode}.csv \
-			--graph-id I4 \
 			--addr http://[::1]:50054 \
 			>$path/tmp_client_${mode}.log 2>&1
 	done
