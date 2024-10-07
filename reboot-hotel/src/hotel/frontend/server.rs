ecf134708dcf043f4e1bc4900f23802450b49774
Weixin Deng <weixin@cs.washington.edu>
2024-10-06 17:42:04 -0700

    chore: Add hotel folder
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

---
+++ reboot-hotel/src/hotel/frontend/server.rs
@@ -0,0 +1,78 @@
+use masa::{
+    frontend, frontend::frontend_server::Frontend, profile, profile::profile_client::ProfileClient,
+    search, search::search_client::SearchClient,
+};
+use tonic::{transport::Channel, Request, Response, Status};
+
+pub mod masa {
+    pub mod frontend {
+        tonic::include_proto!("frontend");
+    }
+    pub mod search {
+        tonic::include_proto!("search");
+    }
+    pub mod profile {
+        tonic::include_proto!("profile");
+    }
+}
+
+pub struct FrontendImpl {
+    search_client: SearchClient<Channel>,
+    profile_client: ProfileClient<Channel>,
+}
+
+impl FrontendImpl {
+    pub async fn new(search_addr: String, profile_addr: String) -> Self {
+        let search_client = SearchClient::connect(search_addr)
+            .await
+            .expect("Failed to connect to search");
+        let profile_client = ProfileClient::connect(profile_addr)
+            .await
+            .expect("Failed to connect to search");
+        FrontendImpl {
+            search_client,
+            profile_client,
+        }
+    }
+}
+
+#[tonic::async_trait]
+impl Frontend for FrontendImpl {
+    async fn handle_search(
+        &self,
+        request: Request<frontend::SearchRequest>,
+    ) -> Result<Response<frontend::SearchResponse>, Status> {
+        let request = request.into_inner();
+
+        let mut search_client = self.search_client.clone();
+        let span_request = search::NearbyRequest { ave: request.ave };
+        let span_response = search_client
+            .handle_nearby(span_request)
+            .await
+            .expect("Failed to call search::nearby");
+        let response = span_response.into_inner();
+
+        // [TODO] Reserve
+        // reserve_client.check_availability()
+        // ReserveRequest { customer, hotel_ids, in_date, out_date, room_number }
+
+        let mut profile_client = self.profile_client.clone();
+        let span_request = profile::ProfileRequest {
+            hotels: response.hotels,
+        };
+        let span_response = profile_client
+            .handle_get_profiles(span_request)
+            .await
+            .expect("Failed to call profile::get_profiles");
+        let response = span_response.into_inner();
+
+        let mut hotels = Vec::new();
+        for profile in response.profiles {
+            hotels.push(frontend::Hotel {
+                name: profile.hotel,
+            });
+        }
+        let response = frontend::SearchResponse { hotels };
+        Ok(Response::new(response))
+    }
+}
