f911a60e325f0e43da031ff977dfa9620457ae54
Weixin Deng <weixin@cs.washington.edu>
2024-10-07 16:25:22 -0700

    refactor: Add client_bench
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- reboot-hotel/src/lib.rs
+++ reboot-hotel/src/lib.rs
@@ -1,5 +1,10 @@
+use std::fs::{self, File};
 use std::future::Future;
+use std::io::Write;
+use std::path::Path;
+use std::time::{SystemTime, UNIX_EPOCH};
 
+use crossbeam_channel::Receiver;
 use env_logger::{Builder, Env};
 use futures_lite::future;
 
@@ -59,5 +64,54 @@ where
 {
     fn execute(&self, fut: F, ddl: PriorityHint) {
         self.ex.spawn_with_prio(fut, ddl).fallible().detach();
+    }
+}
+
+pub fn time_now() -> u64 {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_micros();
+    now as u64
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct Span {
+    request_id: u64,
+    graph_id: String,
+    slo: u64,
+    latency: u64,
+}
+
+#[allow(dead_code)]
+impl Span {
+    pub fn new(request_id: u64, graph_id: String, slo: u64, latency: u64) -> Self {
+        Self {
+            request_id,
+            graph_id,
+            slo,
+            latency,
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub async fn fetch_traces(output: String, trace_rx: Receiver<Span>) {
+    let path = Path::new(&output);
+    if let Some(parent) = path.parent() {
+        if !parent.exists() {
+            fs::create_dir_all(parent).unwrap();
+        }
+    }
+    let mut file = File::create(output).unwrap();
+    writeln!(file, "request_id,graph_id,slo,latency").unwrap();
+    while let Ok(span) = trace_rx.recv() {
+        writeln!(
+            file,
+            "{},{},{},{}",
+            span.request_id, span.graph_id, span.slo, span.latency
+        )
+        .unwrap();
     }
 }
