e6879d85a7c74523051cc9241bfac3406a33037b
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-10-13 11:03:54 -0700

    Update proto, runnable

---
+++ reboot-hotel/src/hotel/recommendation/src/server.rs
@@ -0,0 +1,40 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use recommendation::greeter_server::{Greeter, GreeterServer};
+use recommendation::{HelloReply, HelloRequest};
+
+pub mod recommendation {
+    tonic::include_proto!("recommendation");
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = HelloReply {
+            hotel_ids: vec![format!("require: {}!", request.into_inner().require)],
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
