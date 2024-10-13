698286d6422d5ccc2334c716b8765e9ca8051ac1
Weixin Deng <weixin@cs.washington.edu>
2024-10-13 16:08:23 -0700

    refactor: Use prioritize hint in prio two queue
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- 3rd_party/async-executor/src/lib.rs
+++ 3rd_party/async-executor/src/lib.rs
@@ -444,9 +444,7 @@ where
             //     runnable.deadline().value()
             // );
 
-            use tonic_masa::Prioritize;
-            let prio = runnable.priority();
-            if state.queue.push_with_prio(runnable, prio).is_err() {
+            if state.queue.push(runnable).is_err() {
                 panic!("Failed to push runnable to queue");
             };
             state.notify();
