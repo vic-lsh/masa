044ba0450d38ec5de3e83343852965557c5bec96
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-10-13 11:19:39 -0700

    Change request and reply name in proto

--- reboot-hotel/src/hotel/recommendation/src/client.rs
+++ reboot-hotel/src/hotel/recommendation/src/client.rs
@@ -1,5 +1,5 @@
 use recommendation::recommendation_client::RecommendationClient;
-use recommendation::HelloRequest;
+use recommendation::RecommendationRequest;
 
 pub mod recommendation {
     tonic::include_proto!("recommendation");
@@ -9,13 +9,13 @@ pub mod recommendation {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = RecommendationClient::connect("http://[::1]:50051").await?;
 
-    let request = tonic::Request::new(HelloRequest {
+    let request = tonic::Request::new(RecommendationRequest {
         require: "Tonic".into(),
         lat: 0.1,
         lon: 0.2,
     });
 
-    let response = client.say_hello(request).await?;
+    let response = client.get_recommendations(request).await?;
 
     println!("RESPONSE={:?}", response);
 
