84add78cc90d26bfe788eedb58775cd340589bb1
vic-lsh <shli@cs.washington.edu>
2024-10-15 13:34:43 -0700

    refactor: simplify serving with masa

--- reboot-hotel/src/hotel/rate/main.rs
+++ reboot-hotel/src/hotel/rate/main.rs
@@ -5,14 +5,12 @@ pub mod server;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
-use std::sync::Arc;
 
-use hyper::rt::Exec;
 use structopt::StructOpt;
-use tonic::{masa::AsyncTaskMetadata, transport::Server};
+use tonic::transport::Server;
 
 use config::Config;
-use reboot_hotel::{init_logging, ExecImpl};
+use reboot_hotel::init_logging;
 use server::hotel::rate::rate_server::RateServer;
 use server::RateImpl;
 
@@ -43,22 +41,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     )
     .await?;
 
-    static SMOL_EX: smol::Executor<'static, AsyncTaskMetadata> = smol::Executor::new();
-    let ex = Arc::new(ExecImpl::new(&SMOL_EX));
-    let ex_clone = ex.clone();
-    std::thread::spawn(move || {
-        let rt = tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap();
-        rt.block_on(ex_clone.run());
-    });
-
     let rate_addr = "[::1]:8663".parse().expect("Failed to parse address");
     log::info!("Server listening on {}...", rate_addr);
     Server::builder()
         .add_service(RateServer::new(rate))
-        .serve_with_executor(rate_addr, Exec::Executor(ex))
+        .serve_with_masa(rate_addr)
         .await?;
 
     Ok(())
