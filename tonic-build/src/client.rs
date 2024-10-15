c5c4c37209b95e0bdfae5c5b50863f770e116569
vic-lsh <shli@cs.washington.edu>
2024-10-15 00:24:08 -0700

    feat: scaffold tls parent ctx impl

--- tonic-build/src/client.rs
+++ tonic-build/src/client.rs
@@ -190,13 +190,10 @@ pub(crate) fn generate_internal<T: Service>(
 }
 
 fn generate_get_parent_rpc_ctx(service: &impl Service) -> TokenStream {
-    let server_parent_rpc_ctx = quote::format_ident!("parent_rpc_ctx");
-
     quote! {
         /// Internal. Obtain the parent RPC in which this RPC client stub operates.
         fn get_parent_ctx(&self) -> Option<&'_ P> {
-            let ctx = super::#server_parent_rpc_ctx.get() as *const P;
-            unsafe { ctx.as_ref() }
+            tonic::masa::context::client::get_parent_ctx()
         }
     }
 }
