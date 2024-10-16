8fe5860d3d47a22d8d56ac2fd158f95d3d69c9fe
vic-lsh <shli@cs.washington.edu>
2024-10-15 22:28:11 -0700

    build: remove vendoring in mongodb
    things seem to compile fine -- can revert if things fail b/c of mongo in the future

--- 3rd_party/mongodb/src/test/spec/read_write_concern/operation.rs
+++
@@ -1,7 +0,0 @@
-use crate::test::spec::v2_runner::run_v2_tests;
-
-#[cfg_attr(feature = "tokio-runtime", tokio::test)]
-#[cfg_attr(feature = "async-std-runtime", async_std::test)]
-async fn run() {
-    run_v2_tests(&["read-write-concern", "operation"]).await;
-}
