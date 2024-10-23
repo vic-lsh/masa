815252bbe84a588903c5a469aec9c9b2f6411d27
Weixin Deng <weixin@cs.washington.edu>
2024-10-23 14:23:10 -0700

    refactor: Unify plots
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- reboot-hotel/scripts/plots/plot_error.py
+++ reboot-hotel/scripts/plots/plot_error.py
@@ -2,29 +2,26 @@ import json
 from typing import *
 
 import pandas as pd
-from plot_core import GRAPH_IDS, plot_error  # type: ignore
+from plot_core import GRAPH_IDS, parse_args, plot_error  # type: ignore
 
-cfg = json.load(open("gen_config.json"))
+args = parse_args()
+cfg = json.load(open(f"{args.path}/gen_config.json"))
 
 for r in range(cfg["Repeats"]):
     rps_to_results: List[Dict[str, Any]] = []
 
     for rps in cfg["Rps"]:
-        file = f"r{rps}_{r}.csv"
+        file = f"{args.path}/r{rps}_{r}.csv"
         df = pd.read_csv(file)
         for graph_id in GRAPH_IDS:
             result = {}
             result["rps"] = rps
             result["graph_id"] = graph_id
 
-            df_filtered = df[
-                (df["graph_id"] == graph_id) & (df["error"] != "/None")
-            ]
+            df_filtered = df[(df["graph_id"] == graph_id) & (df["error"] != "/None")]
             result["error_all"] = round(len(df_filtered) / cfg["DurationSecs"])
 
-            df_filtered = df[
-                (df["graph_id"] == graph_id) & (df["error"] == "/LoadGen")
-            ]
+            df_filtered = df[(df["graph_id"] == graph_id) & (df["error"] == "/LoadGen")]
             result["error_load_gen"] = round(len(df_filtered) / cfg["DurationSecs"])
 
             df_filtered = df[
@@ -41,4 +38,4 @@ for r in range(cfg["Repeats"]):
 
             rps_to_results.append(result)
 
-    plot_error(rps_to_results, f"fig_error_rps_{r}.png")
+    plot_error(rps_to_results, f"{args.path}/fig_error_rps_{r}.png")
--- reboot-hotel/snippets/prio_global_two/plot.sh
+++ reboot-hotel/snippets/prio_global_two/plot.sh
@@ -1,3 +1,11 @@
-python plot_goodput.py
-python plot_throughput.py
-python plot_error.py
+python ~/Masa-Lo-Ding/reboot-hotel/scripts/plots/plot_goodput.py \
+	--path ~/Masa-Lo-Ding/reboot-hotel/snippets/prio_global_two
+echo "Goodput done"
+
+python ~/Masa-Lo-Ding/reboot-hotel/scripts/plots/plot_throughput.py \
+	--path ~/Masa-Lo-Ding/reboot-hotel/snippets/prio_global_two
+echo "Throughput done"
+
+python ~/Masa-Lo-Ding/reboot-hotel/scripts/plots/plot_error.py \
+	--path ~/Masa-Lo-Ding/reboot-hotel/snippets/prio_global_two
+echo "Error done"
