4b1c632d34ac7862963fc06c14d532e5f7d1b88d
Weixin Deng <weixin@cs.washington.edu>
2024-10-23 16:06:15 -0700

    test: Add bar plots
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- reboot-hotel/scripts/plots/plot_error.py
+++ reboot-hotel/scripts/plots/plot_error.py
@@ -2,7 +2,12 @@ import json
 from typing import *
 
 import pandas as pd
-from plot_core import GRAPH_IDS, parse_args, plot_error  # type: ignore
+from plot_core import (  # type: ignore
+    GRAPH_IDS,
+    parse_args,
+    plot_error_bar,
+    plot_error_line,
+)
 
 args = parse_args()
 cfg = json.load(open(f"{args.path}/gen_config.json"))
@@ -38,4 +43,7 @@ for r in range(cfg["Repeats"]):
 
             rps_to_results.append(result)
 
-    plot_error(rps_to_results, f"{args.path}/fig_error_rps_{r}.png", args.mode)
+    plot_error_line(
+        rps_to_results, f"{args.path}/fig_error_line_rps_{r}.png", args.mode
+    )
+    plot_error_bar(rps_to_results, f"{args.path}/fig_error_bar_rps_{r}.png", args.mode)
