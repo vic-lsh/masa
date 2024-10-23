34d3896a0d321d689761620ec33a11143b1ccbc2
Weixin Deng <weixin@cs.washington.edu>
2024-10-23 14:30:05 -0700

    chore: Plot styles
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- reboot-hotel/snippets/prio_global/plot.sh
+++ reboot-hotel/snippets/prio_global/plot.sh
@@ -1,11 +1,17 @@
-python ~/Masa-Lo-Ding/reboot-hotel/scripts/plots/plot_goodput.py \
-	--path ~/Masa-Lo-Ding/reboot-hotel/snippets/prio_global
+reboot_hotel=~/Masa-Lo-Ding/reboot-hotel
+mode=prio_global
+
+python $reboot_hotel/scripts/plots/plot_goodput.py \
+	--path $reboot_hotel/snippets/$mode \
+	--mode $mode
 echo "Goodput done"
 
-python ~/Masa-Lo-Ding/reboot-hotel/scripts/plots/plot_throughput.py \
-	--path ~/Masa-Lo-Ding/reboot-hotel/snippets/prio_global
+python $reboot_hotel/scripts/plots/plot_throughput.py \
+	--path $reboot_hotel/snippets/$mode \
+	--mode $mode
 echo "Throughput done"
 
-python ~/Masa-Lo-Ding/reboot-hotel/scripts/plots/plot_error.py \
-	--path ~/Masa-Lo-Ding/reboot-hotel/snippets/prio_global
+python $reboot_hotel/scripts/plots/plot_error.py \
+	--path $reboot_hotel/snippets/$mode \
+	--mode $mode
 echo "Error done"
