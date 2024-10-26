aab2e839b9d5e67f30fa6c846c44aad7525d8467
vic-lsh <shli@cs.washington.edu>
2024-10-26 09:37:20 -0700

    feat: use recommendation service's original test data

--- reboot-hotel/src/hotel/recommendation/main.rs
+++ reboot-hotel/src/hotel/recommendation/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("hello world!")
+#[path = "../config.rs"]
+mod config;
+mod db;
+mod server;
+
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
+use std::sync::Arc;
+
+use hyper::rt::Exec;
+use structopt::StructOpt;
+use tonic::{masa::AsyncTaskMetadata, transport::Server};
+
+use config::HotelConfig;
+use reboot_hotel::{init_logging, ExecImpl};
+use server::hotel::recommendation::recommendation_server::RecommendationServer;
+use server::RecommendationImpl;
+
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(about = "Hotel Args")]
+pub struct Args {
+    #[structopt(short, long, required = true)]
+    pub config: PathBuf,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    init_logging();
+
+    let args = Args::from_args();
+    let cfg: HotelConfig = {
+        let file = File::open(args.config).expect("Failed to open file");
+        let reader = BufReader::new(file);
+        serde_json::from_reader(reader)?
+    };
+    log::info!("Hotel config: {:?}", cfg);
+
+    let rec = RecommendationImpl::new();
+
+    static SMOL_EX: smol::Executor<'static, AsyncTaskMetadata> = smol::Executor::new();
+    let ex = Arc::new(ExecImpl::new(&SMOL_EX));
+    let ex_clone = ex.clone();
+    std::thread::spawn(move || {
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        rt.block_on(ex_clone.run());
+    });
+
+    let rec_addr = "[::1]:8665".parse().expect("Failed to parse address");
+    log::info!("Server listening on {}...", rec_addr);
+    Server::builder()
+        .add_service(RecommendationServer::new(rec))
+        .serve_with_executor(rec_addr, Exec::Executor(ex))
+        .await?;
+
+    Ok(())
 }
