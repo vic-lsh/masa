5ff488dbf7d53553858f9d1858f02e9467ba4799
Weixin Deng <weixin@cs.washington.edu>
2024-10-28 13:37:14 -0700

    chore: Add ftitle alias
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- reboot-hotel/snippets/prio_global/plot.sh
+++ reboot-hotel/snippets/prio_global/plot.sh
@@ -3,20 +3,24 @@ mode=prio_global
 
 python $reboot_hotel/scripts/plots/plot_goodput.py \
 	--path $reboot_hotel/snippets/$mode \
-	--mode $mode
+	--mode $mode \
+	--alias no_early_return
 echo "Goodput done"
 
 python $reboot_hotel/scripts/plots/plot_throughput.py \
 	--path $reboot_hotel/snippets/$mode \
-	--mode $mode
+	--mode $mode \
+	--alias no_early_return
 echo "Throughput done"
 
 python $reboot_hotel/scripts/plots/plot_error.py \
 	--path $reboot_hotel/snippets/$mode \
-	--mode $mode
+	--mode $mode \
+	--alias no_early_return
 echo "Error done"
 
 python $reboot_hotel/scripts/plots/plot_tail.py \
 	--path $reboot_hotel/snippets/$mode \
-	--mode $mode
+	--mode $mode \
+	--alias no_early_return
 echo "Tail done"
