ad56c0e7c2fbd0d3b4df1b1a76dadff5809a1613
Weixin Deng <weixin@cs.washington.edu>
2024-10-30 16:21:59 -0700

    test: Add results with latency tracker
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- reboot-hotel/src/hotel/rate/server.rs
+++ reboot-hotel/src/hotel/rate/server.rs
@@ -220,7 +220,7 @@ impl HotelManager {
 
 pub struct RateImpl {
     manager: HotelManager,
-    lat: Mutex<LatencyTracker>,
+    // lat: Mutex<LatencyTracker>,
 }
 
 impl RateImpl {
@@ -232,7 +232,7 @@ impl RateImpl {
         cache_miss_rate: u32,
         db_addr: String,
     ) -> Result<Self, Box<dyn Error>> {
-        let lat = Mutex::new(LatencyTracker::new("RateSvc".to_string(), 512));
+        // let lat = Mutex::new(LatencyTracker::new("RateSvc".to_string(), 512));
         let manager = HotelManager::new(
             hotels,
             payload,
@@ -242,7 +242,7 @@ impl RateImpl {
             db_addr,
         )
         .await?;
-        let rate = RateImpl { manager, lat };
+        let rate = RateImpl { manager };
         Ok(rate)
     }
 }
@@ -253,7 +253,7 @@ impl Rate for RateImpl {
         &self,
         request: Request<rate::RateRequest>,
     ) -> Result<Response<rate::RateResponse>, Status> {
-        let start = Instant::now();
+        // let start = Instant::now();
         // let ctx = request.metadata().get_ctx("ctx").unwrap();
         let request = request.into_inner();
         let hotels = self.manager.fetch_mixture(request.hotels).await;
@@ -267,13 +267,13 @@ impl Rate for RateImpl {
         }
         let response = rate::RateResponse { plans };
         log::info!("response: {:?}", response);
-        let end = start.elapsed();
-        {
-            self.lat
-                .lock()
-                .unwrap()
-                .track(end.as_micros().try_into().unwrap());
-        }
+        // let end = start.elapsed();
+        // {
+        //     self.lat
+        //         .lock()
+        //         .unwrap()
+        //         .track(end.as_micros().try_into().unwrap());
+        // }
         Ok(Response::new(response))
     }
 }
