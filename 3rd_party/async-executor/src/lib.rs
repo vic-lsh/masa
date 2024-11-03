493aaf05fa00c77f3d011ab3f96528e38ea0687d
Weixin Deng <weixin@cs.washington.edu>
2024-11-02 20:41:33 -0700

    test: Add results after fix queue
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- 3rd_party/async-executor/src/lib.rs
+++ 3rd_party/async-executor/src/lib.rs
@@ -768,7 +768,7 @@ type GlobalQueue<T> = queue::MutexFifoTwoQueue<T>;
 #[cfg(any(feature = "prio_global", feature = "prio_local"))]
 type GlobalQueue<T> = queue::MutexPriorityQueue<T>;
 #[cfg(any(feature = "prio_global_two", feature = "prio_local_two"))]
-type GlobalQueue<T> = queue::MutexPriorityTwoQueue<T>;
+type GlobalQueue<T> = queue::MutexPriorityQueue<T>;
 
 // [NOTE] The original implementation uses a concurrent queue for the global queue.
 // type GlobalQueue<T> = queue::ConcurrentFifoQueue<T>;
