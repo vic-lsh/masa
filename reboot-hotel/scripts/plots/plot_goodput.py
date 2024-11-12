40cd91cd0885e38ec9868fe3c1468fbadca7907a
Weixin Deng <weixin@cs.washington.edu>
2024-11-11 22:02:00 -0800

    chore: Plot styles
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- reboot-hotel/scripts/plots/plot_goodput.py
+++ reboot-hotel/scripts/plots/plot_goodput.py
@@ -32,11 +32,11 @@ for r in range(cfg["Repeats"]):
 
         rps_to_results.append(result)
 
-    apis = " ".join(cfg["Apis"]).lower()
     plot_goodput_bar(
+        cfg["Apis"],
+        args.mode,
         rps_to_results,
         f"{args.path}/fig_goodput_rps_bar_{r}.png",
-        f"{args.mode} {apis}",
     )
     if len(cfg["Apis"]) == 1:
         continue
@@ -61,7 +61,7 @@ for r in range(cfg["Repeats"]):
 
     plot_goodput_apis_bar(
         cfg["Apis"],
+        args.mode,
         rps_to_results,
         f"{args.path}/fig_goodput_apis_rps_bar_{r}.png",
-        args.mode,
     )
