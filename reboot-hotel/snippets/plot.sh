b7b7efbcbf68911434cb6eb63a7e72fb4f184fd7
Weixin Deng <weixin@cs.washington.edu>
2024-11-11 21:36:54 -0800

    test: Plots for fifo/e2e search/reservation
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- reboot-hotel/snippets/plot.sh
+++ reboot-hotel/snippets/plot.sh
@@ -6,14 +6,29 @@ if [[ "$pwd" != */reboot-hotel ]]; then
     exit 1
 fi
 
-echo "Plotting all_cmp..."
-$pwd/snippets/all_cmp/plot.sh
+echo "Plotting fifo_search..."
+$pwd/snippets/fifo_search/plot.sh
 
-echo "Plotting fifo_infra..."
-$pwd/snippets/fifo_infra/plot.sh
+echo "Plotting fifo_reservation..."
+$pwd/snippets/fifo_reservation/plot.sh
 
-echo "Plotting prio_global..."
-$pwd/snippets/prio_global/plot.sh
+echo "Plotting e2e_search..."
+$pwd/snippets/e2e_search/plot.sh
 
-echo "Plotting prio_global_early..."
-$pwd/snippets/prio_global_early/plot.sh
+echo "Plotting e2e_reservation..."
+$pwd/snippets/e2e_reservation/plot.sh
+
+# echo "Plotting all_cmp..."
+# $pwd/snippets/all_cmp/plot.sh
+
+# echo "Plotting fifo..."
+# $pwd/snippets/fifo/plot.sh
+
+# echo "Plotting fifo_infra..."
+# $pwd/snippets/fifo_infra/plot.sh
+
+# echo "Plotting prio_global..."
+# $pwd/snippets/prio_global/plot.sh
+
+# echo "Plotting prio_global_early..."
+# $pwd/snippets/prio_global_early/plot.sh
