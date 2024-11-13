bfa9fd15181e1983f8ca58052899c56b8c8bb11d
Weixin Deng <weixin@cs.washington.edu>
2024-11-12 17:31:45 -0800

    refactor: Snippets version
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- reboot-hotel/snippets/single/plot.sh
+++ reboot-hotel/snippets/single/plot.sh
@@ -7,35 +7,35 @@ if [[ "$pwd" != */reboot-hotel ]]; then
 fi
 snippets=snippets/single
 
-# echo "Plotting fifo_search..."
-# $pwd/$snippets/fifo_search/plot.sh
+echo "Plotting fifo_search..."
+$pwd/$snippets/fifo_search/plot.sh
 
-# echo "Plotting fifo_reservation..."
-# $pwd/$snippets/fifo_reservation/plot.sh
+echo "Plotting fifo_reservation..."
+$pwd/$snippets/fifo_reservation/plot.sh
 
-# echo "Plotting e2e_search..."
-# $pwd/$snippets/e2e_search/plot.sh
+echo "Plotting e2e_search..."
+$pwd/$snippets/e2e_search/plot.sh
 
-# echo "Plotting e2e_reservation..."
-# $pwd/$snippets/e2e_reservation/plot.sh
+echo "Plotting e2e_reservation..."
+$pwd/$snippets/e2e_reservation/plot.sh
 
-# echo "Plotting e2e_er_search..."
-# $pwd/$snippets/e2e_er_search/plot.sh
+echo "Plotting e2e_er_search..."
+$pwd/$snippets/e2e_er_search/plot.sh
 
-# echo "Plotting e2e_er_reservation..."
-# $pwd/$snippets/e2e_er_reservation/plot.sh
+echo "Plotting e2e_er_reservation..."
+$pwd/$snippets/e2e_er_reservation/plot.sh
 
-# echo "Plotting local_search..."
-# $pwd/$snippets/local_search/plot.sh
+echo "Plotting local_search..."
+$pwd/$snippets/local_search/plot.sh
 
-# echo "Plotting local_reservation..."
-# $pwd/$snippets/local_reservation/plot.sh
+echo "Plotting local_reservation..."
+$pwd/$snippets/local_reservation/plot.sh
 
-# echo "Plotting local_er_search..."
-# $pwd/$snippets/local_er_search/plot.sh
+echo "Plotting local_er_search..."
+$pwd/$snippets/local_er_search/plot.sh
 
-# echo "Plotting local_er_reservation..."
-# $pwd/$snippets/local_er_reservation/plot.sh
+echo "Plotting local_er_reservation..."
+$pwd/$snippets/local_er_reservation/plot.sh
 
 echo "Plotting cmp..."
 $pwd/$snippets/cmp/plot.sh
