91493ceda0440737cf57a7c1278e0cec9bc3671d
Weixin Deng <weixin@cs.washington.edu>
2024-11-12 20:03:21 -0800

    refactor: Snippets version
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- reboot-hotel/snippets/search_reservation/fifo/plot.sh
+++ reboot-hotel/snippets/search_reservation/fifo/plot.sh
@@ -1,28 +1,15 @@
-reboot_hotel=~/Masa-Lo-Ding/reboot-hotel
-snippets=snippets/search_reservation
-path=fifo
-data=tmp_1112
+#!/bin/bash
+
+pwd=$(pwd)
+if [[ "$pwd" != */reboot-hotel ]]; then
+	echo "Error: plese run in the reboot-hotel directory" >&2
+	exit 1
+fi
+snippets=snippets/search_reservation/fifo
 mode=fifo
+data=tmp_1112
 
 echo "Plotting goodput..."
-python3 $reboot_hotel/scripts/plots/plot_goodput.py \
-	--path $reboot_hotel/$snippets/$path \
-	--data $data \
-	--mode $mode
-
-# [TODO] Change $mode to $path.
-
-# echo "Plotting throughput..."
-# python3 $reboot_hotel/scripts/plots/plot_throughput.py \
-# 	--path $reboot_hotel/$snippets/$mode \
-# 	--mode $mode
-
-# echo "Plotting error..."
-# python3 $reboot_hotel/scripts/plots/plot_error.py \
-# 	--path $reboot_hotel/$snippets/$mode \
-# 	--mode $mode
-
-# echo "Plotting tail..."
-# python3 $reboot_hotel/scripts/plots/plot_tail.py \
-# 	--path $reboot_hotel/$snippets/$mode \
-# 	--mode $mode
+python3 $pwd/scripts/plots/plot_goodput.py \
+	--mode $mode \
+	--path $pwd/$snippets/$data \
--- reboot-hotel/snippets/search_reservation/plot.sh
+++ reboot-hotel/snippets/search_reservation/plot.sh
@@ -7,20 +7,20 @@ if [[ "$pwd" != */reboot-hotel ]]; then
 fi
 snippets=snippets/search_reservation
 
-# echo "Plotting fifo..."
-# $pwd/$snippets/fifo/plot.sh
+echo "Plotting fifo..."
+$pwd/$snippets/fifo/plot.sh
 
-# echo "Plotting e2e..."
-# $pwd/$snippets/e2e/plot.sh
+echo "Plotting e2e..."
+$pwd/$snippets/e2e/plot.sh
 
-# echo "Plotting e2e_er..."
-# $pwd/$snippets/e2e_er/plot.sh
+echo "Plotting e2e_er..."
+$pwd/$snippets/e2e_er/plot.sh
 
-# echo "Plotting local..."
-# $pwd/$snippets/local/plot.sh
+echo "Plotting local..."
+$pwd/$snippets/local/plot.sh
 
-# echo "Plotting local_er..."
-# $pwd/$snippets/local_er/plot.sh
+echo "Plotting local_er..."
+$pwd/$snippets/local_er/plot.sh
 
 echo "Plotting cmp..."
 $pwd/$snippets/cmp/plot.sh
