f1324bbb7ccd41c9c200989c088fe58b15782b95
Weixin Deng <weixin@cs.washington.edu>
2024-11-14 14:48:33 -0800

    refactor: Plot scripts
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- reboot-hotel/snippets/search_reservation/local_p75/plot.sh
+++ reboot-hotel/snippets/search_reservation/local_p75/plot.sh
@@ -5,11 +5,13 @@ if [[ "$pwd" != */reboot-hotel ]]; then
 	echo "Error: plese run in the reboot-hotel directory" >&2
 	exit 1
 fi
-snippets=snippets/search_reservation/local_p75
+
+folder=snippets/search_reservation
+snippet=local_p75
 mode=local
 data=tmp_1113
 
 echo "Plotting goodput..."
 python3 $pwd/scripts/plots/plot_goodput.py \
 	--mode $mode \
-	--path $pwd/$snippets/$data
+	--path $pwd/$folder/$snippet/$data
