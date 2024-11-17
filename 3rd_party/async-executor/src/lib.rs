ce75617633ab0d2d7d0c1f93c7a578704346fbb4
vic-lsh <shli@cs.washington.edu>
2024-11-17 13:46:27 -0800

    change async-executor threads to 4

--- 3rd_party/async-executor/src/lib.rs
+++ 3rd_party/async-executor/src/lib.rs
@@ -74,7 +74,7 @@ fn get_static_ex() -> &'static Executor<'static> {
         .is_ok()
     {
         // [TODO] make thread pool size configurable
-        const N_THRS: usize = 16;
+        const N_THRS: usize = 4;
         for _ in 0..N_THRS {
             // std::thread::spawn(|| future::block_on(drive_runtime(&__STATIC_EX)));
             std::thread::spawn(move || {
