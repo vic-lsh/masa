7f8e715819fd35dd85a7e2b8783a8a786e4e3b37
Weixin Deng <weixin@cs.washington.edu>
2024-11-18 17:09:03 -0800

    test: Simplify plots
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- reboot-hotel/scripts/plots/plot_tail_cmp.py
+++ reboot-hotel/scripts/plots/plot_tail_cmp.py
@@ -47,6 +47,9 @@ for tail in ["mean", "p50", "p90", "p95", "p99"]:
         f"{args.path}/fig_{tail}_total_rps_cmp_{modes_str}_{r}.png",
     )
 
+if len(args.apis) == 1:
+    exit()
+
 for api in cfg["Apis"]:
     rps_to_results = []
 
