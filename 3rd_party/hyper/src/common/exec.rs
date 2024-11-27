9b42280db870980a731964e1e186ed905b4806e8
vic-lsh <shli@cs.washington.edu>
2024-11-27 14:07:49 -0800

    feat: add tokio spawn_with_prio api

--- 3rd_party/hyper/src/common/exec.rs
+++ 3rd_party/hyper/src/common/exec.rs
@@ -63,9 +63,10 @@ impl Exec {
                 }
             }
             Exec::Masa => {
-                async_executor::spawn_with_prio(fut, prio)
-                    .fallible()
-                    .detach();
+                tokio::task::spawn_with_prio(fut, prio);
+                // async_executor::spawn_with_prio(fut, prio)
+                //     .fallible()
+                //     .detach();
             }
             Exec::Executor(ref e) => {
                 e.execute(Box::pin(fut), prio);
