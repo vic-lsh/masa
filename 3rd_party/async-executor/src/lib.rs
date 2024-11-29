598f97ebcc16a5b4defa9b990042b564cb585409
Weixin Deng <weixin@cs.washington.edu>
2024-11-29 14:41:22 -0800

    feat: Test track unified future span
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- 3rd_party/async-executor/src/lib.rs
+++ 3rd_party/async-executor/src/lib.rs
@@ -75,9 +75,10 @@ fn get_static_ex() -> &'static Executor<'static> {
         .is_ok()
     {
         // [TODO:Vic] make thread pool size configurable.
-        let num_cpus = num_cpus::get();
-        println!("Number of CPU cores: {}", num_cpus);
-        for _ in 0..num_cpus {
+        // let num_cpus = num_cpus::get();
+        // log::warn!("Number of CPU cores: {}", num_cpus);
+        // for _ in 0..num_cpus {
+        for _ in 0..1 {
             std::thread::spawn(move || {
                 let rt = tokio::runtime::Builder::new_current_thread()
                     .enable_all()
