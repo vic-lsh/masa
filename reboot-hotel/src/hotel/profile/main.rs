e4caaf0a70b70305bbdae65cf0c37a89fbdbd068
vic-lsh <shli@cs.washington.edu>
2024-12-02 18:12:06 -0800

    update hotel to use single-thread tokio runtime

--- reboot-hotel/src/hotel/profile/main.rs
+++ reboot-hotel/src/hotel/profile/main.rs
@@ -22,8 +22,8 @@ pub struct Args {
     pub config: PathBuf,
 }
 
-// #[tokio::main(flavor = "current_thread")]
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
+//#[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     init_logging();
 
