76fa2f660b7c34b3cb9d1f25b5f3f17a02d310fb
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2025-03-02 00:29:03 +0000

    script: use docker containers to run search-reservation

--- apps/hotel/snippets/search-reservation/run_snippet.sh
+++ apps/hotel/snippets/search-reservation/run_snippet.sh
@@ -9,32 +9,32 @@ fi
 folder=snippets/search-reservation
 data=tmp_1129_gcp
 
-# echo "Running fifo..."
-# $pwd/scripts/local/run_all.sh \
-# 	--rust-log warn \
-# 	--tracker-capacity 1024 \
-# 	--pctl-deadline 50 \
-# 	--pctl-latest-exec 50 \
-# 	--cargo-features fifo \
-# 	--gen-config $folder/gen_config.json \
-# 	--hotel-config $folder/hotel_config.json \
-# 	--output-path $folder/fifo/$data \
-# 	--repeats 1
-
-echo "Running fifo_er..."
-$pwd/scripts/local/run_all.sh \
+echo "Running fifo..."
+$pwd/scripts/local/run_all_docker.sh \
 	--rust-log warn \
 	--tracker-capacity 1024 \
 	--pctl-deadline 50 \
 	--pctl-latest-exec 50 \
-	--cargo-features fifo_early \
+	--cargo-features fifo \
 	--gen-config $folder/gen_config.json \
 	--hotel-config $folder/hotel_config.json \
-	--output-path $folder/fifo_er/$data \
+	--output-path $folder/fifo/$data \
 	--repeats 1
 
+# echo "Running fifo_er..."
+# $pwd/scripts/local/run_all_docker.sh \
+# 	--rust-log warn \
+# 	--tracker-capacity 1024 \
+# 	--pctl-deadline 50 \
+# 	--pctl-latest-exec 50 \
+# 	--cargo-features fifo_early \
+# 	--gen-config $folder/gen_config.json \
+# 	--hotel-config $folder/hotel_config.json \
+# 	--output-path $folder/fifo_er/$data \
+# 	--repeats 1
+
 # echo "Running e2e..."
-# $pwd/scripts/local/run_all.sh \
+# $pwd/scripts/local/run_all_docker.sh \
 # 	--rust-log warn \
 # 	--tracker-capacity 1024 \
 # 	--pctl-deadline 50 \
@@ -46,7 +46,7 @@ $pwd/scripts/local/run_all.sh \
 # 	--repeats 1
 
 # echo "Running e2e_er..."
-# $pwd/scripts/local/run_all.sh \
+# $pwd/scripts/local/run_all_docker.sh \
 # 	--rust-log warn \
 # 	--tracker-capacity 1024 \
 # 	--pctl-deadline 50 \
@@ -58,7 +58,7 @@ $pwd/scripts/local/run_all.sh \
 # 	--repeats 1
 
 # echo "Running local..."
-# $pwd/scripts/local/run_all.sh \
+# $pwd/scripts/local/run_all_docker.sh \
 # 	--rust-log warn \
 # 	--tracker-capacity 1024 \
 # 	--pctl-deadline 50 \
@@ -70,7 +70,7 @@ $pwd/scripts/local/run_all.sh \
 # 	--repeats 1
 
 # echo "Running local_er..."
-# $pwd/scripts/local/run_all.sh \
+# $pwd/scripts/local/run_all_docker.sh \
 # 	--rust-log warn \
 # 	--tracker-capacity 1024 \
 # 	--pctl-deadline 50 \
