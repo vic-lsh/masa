ed12eb29af30f40e65a84763544617e7f2a0b185
aellison <andreas.ellison@gmail.com>
2025-03-19 14:13:44 -0700

    create trait for expressing an implementation of the triple of Server, Parent and Client traits

--- libs/tonic/tests/masa_integration_tests/tests/masa_context.rs
+++ libs/tonic/tests/masa_integration_tests/tests/masa_context.rs
@@ -15,42 +15,32 @@ use masa_integration_tests::pb::{
     parent_service_server::*,
     Input1, Input2, Output1, Output2,
 };
-use tonic::{
-    masa::{ClientHooks, ParentHooks, ServerHooks},
-    transport::Server,
-    GrpcMethod, Request, Response, Status,
-};
+use tonic::{masa::PrioritySelector, transport::Server, GrpcMethod, Request, Response, Status};
 use tonic_masa::PriorityHint;
 
-struct ParentSvc<S, C, P> {
+struct ParentSvc {
     child_addr: &'static str,
     fanout_factor: usize,
-    _s_ty: PhantomData<S>,
-    _c_ty: PhantomData<C>,
-    _p_ty: PhantomData<P>,
 }
 
-impl<S, C, P> ParentSvc<S, C, P> {
+impl ParentSvc {
     fn new(child_addr: &'static str, fanout_factor: usize) -> Self {
         Self {
             child_addr,
             fanout_factor,
-            _s_ty: PhantomData,
-            _c_ty: PhantomData,
-            _p_ty: PhantomData,
         }
     }
 }
 
 #[tonic::async_trait]
-impl<S, C, P> ParentService for ParentSvc<S, C, P>
+impl<P> ParentService for ParentSvc
 where
-    S: ServerHooks,
-    C: ClientHooks + Send + Sync + 'static,
-    P: ParentHooks<C, S> + 'static,
+    P: PrioritySelector,
+    P::ClientContext: Send + Sync + 'static,
+    P::ParentContext: 'static,
 {
     async fn rpc(&self, _req: Request<Input1>) -> Result<Response<Output1>, Status> {
-        let mut client = ChildServiceClient::<_, S, C, P>::connect_with_custom_context(format!(
+        let mut client = ChildServiceClient::<_, P>::connect_with_custom_context(format!(
             "http://{}",
             self.child_addr
         ))
@@ -66,7 +56,7 @@ where
     }
 
     async fn fanout_rpc(&self, _req: Request<Input1>) -> Result<Response<Output1>, Status> {
-        let client = ChildServiceClient::<_, S, C, P>::connect_with_custom_context(format!(
+        let client = ChildServiceClient::<_, P>::connect_with_custom_context(format!(
             "http://{}",
             self.child_addr
         ))
@@ -139,21 +129,19 @@ impl ClientHooks for MockChildCtx {
     }
 }
 
-async fn make_parent_child_svcs<S, C, P>(
+async fn make_parent_child_svcs<P>(
     parent_svc_addr: &'static str,
     child_svc_addr: &'static str,
     fanout_factor: usize,
 ) -> (tokio::task::JoinHandle<()>, tokio::task::JoinHandle<()>)
 where
-    S: ServerHooks,
-    C: ClientHooks + Send + Sync + 'static,
-    P: ParentHooks<C, S> + 'static,
+    P: PrioritySelector,
+    P::ClientContext: Send + Sync + 'static,
+    P::ParentContext: 'static,
 {
     let child_svc = tokio::spawn(async {
         Server::builder()
-            .add_service(ChildServiceServer::<_, S, C, P>::with_custom_context(
-                ChildSvc,
-            ))
+            .add_service(ChildServiceServer::<_, P>::with_custom_context(ChildSvc))
             .serve_with_executor(
                 child_svc_addr.parse().unwrap(),
                 Exec::Executor(Arc::new(ExecImpl)),
@@ -164,8 +152,8 @@ where
 
     let parent_svc = tokio::spawn(async move {
         Server::builder()
-            .add_service(ParentServiceServer::<_, S, C, P>::with_custom_context(
-                ParentSvc::<S, C, P>::new(child_svc_addr, fanout_factor),
+            .add_service(ParentServiceServer::<_, P>::with_custom_context(
+                ParentSvc::<P>::new(child_svc_addr, fanout_factor),
             ))
             .serve_with_executor(
                 parent_svc_addr.parse().unwrap(),
