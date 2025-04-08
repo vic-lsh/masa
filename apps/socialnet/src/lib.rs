0f444e2867df8802efdbdddda7ba8e69e1a89819
yusufmohammed <yusufrm@cs.washington.edu>
2025-04-07 22:36:51 -0700

    Added DB for URL_Shorten_Service

--- apps/socialnet/src/lib.rs
+++ apps/socialnet/src/lib.rs
@@ -1,6 +1,12 @@
 pub mod media {
     tonic::include_proto!("media");
-
     pub mod client;
     pub mod server;
+}
+
+pub mod url_shorten {
+    tonic::include_proto!("url_shorten");
+    pub mod server;
+    pub mod client;
+    pub mod db;
 }
