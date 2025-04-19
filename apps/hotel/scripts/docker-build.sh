8035a57fa7aa0adfac7dec79b7eddcef930d5614
vic-lsh <shli@cs.washington.edu>
2025-04-19 16:08:46 -0700

    rm parallel build

--- apps/hotel/scripts/docker-build.sh
+++ apps/hotel/scripts/docker-build.sh
@@ -17,7 +17,6 @@ if [[ "$pwd" != */apps/hotel ]]; then
 fi
 
 features=""
-parallel=0
 
 # Parse arguments
 while [[ $# -gt 0 ]]; do
@@ -25,10 +24,6 @@ while [[ $# -gt 0 ]]; do
     --features)
         features="$2"
         shift 2
-        ;;
-    --par)
-        parallel=1
-        shift 1
         ;;
     *)
         echo "Unknown argument: $1"
@@ -44,85 +39,13 @@ fi
 
 echo "Building all hotel services. Feature flags: $features."
 
-if [[ -z "$features" ]]; then
-    cmd="cargo build --release"
-else
-    cmd="cargo build --release --features $features"
-fi
-output=$(eval "$cmd" 2>&1)
-exit_code=$?
-if [ $exit_code -ne 0 ]; then
-    echo "Build failed with exit code $exit_code"
-    echo "Output:"
-    echo "$output"
-    exit $exit_code
-fi
+echo "Building docker images sequentially."
 
-if [ $parallel -eq 0 ]; then
-    echo "Building docker images sequentially."
-
-    set -e
-    for svc in "${services[@]}"; do
+set -e
+for svc in "${services[@]}"; do
+    if [[ -z "$features" ]]; then
         ./scripts/docker-build-svc.sh --binary $svc
-    done
-
-else
-    echo "Building docker images in parallel."
-
-    declare -A svc_pid_map
-
-    declare -a pids
-
-    # Function to handle errors
-    handle_error() {
-        local pid=$1
-        local exit_status=$2
-        # Look up the original svcing that caused the error
-        for svc in "${!svc_pid_map[@]}"; do
-            if [ "${svc_pid_map[$svc]}" == "$pid" ]; then
-                echo "Failed to build docker image for $svc" >&2
-                break
-            fi
-        done
-    }
-
-    build_docker_img() {
-        local svc=$1
-        local pid=$$
-        if ./scripts/docker-build-svc.sh --binary "$svc"; then
-            #echo "Process $pid completed successfully"
-            return 0
-        else
-            echo "Process $pid failed"
-            return 1
-        fi
-    }
-
-    # Build docker images in parallel
-    for svc in "${services[@]}"; do
-        build_docker_img "$svc" >/dev/null 2>&1 &
-        pid=$!
-        pids+=($pid)
-        svc_pid_map[$svc]=$pid
-        echo "Started building docker image for service '$svc' at pid $pid."
-    done
-
-    failed=0
-
-    # Wait for all processes to complete and check their exit status
-    for pid in "${pids[@]}"; do
-        if ! wait $pid; then
-            handle_error $pid $?
-            failed=1
-        fi
-    done
-
-    echo "-----------------------------------"
-    if [ $failed -eq 1 ]; then
-        echo "One or more processes failed" >&2
-        exit 1
     else
-        echo "All processes completed successfully"
-        exit 0
+        ./scripts/docker-build-svc.sh --binary $svc --features $features
     fi
-fi
+done
