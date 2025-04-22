42905b7b160401e427aca3d149506ae44d0bbd6b
jiexiao <jiexiao@cs.washington.edu>
2025-04-22 18:00:15 +0000

    Integrate UserMention service with TextService

--- apps/socialnet/src/text_service/server.rs
+++ apps/socialnet/src/text_service/server.rs
@@ -8,18 +8,38 @@ use regex::Regex;
 
 use tonic::{transport::Server, Request, Response, Status};
 
-use text_service::text_service_server::{TextService, TextServiceServer};
-use text_service::{TextReply, TextRequest};
+use text_svc::text_service::text_service_server::{TextService, TextServiceServer};
+use text_svc::text_service::{TextReply, TextRequest};
+use text_svc::user_mention_service::{user_mention_service_client::UserMentionServiceClient, 
+                                        ComposeUserMentionRequest, UserMention};
 
-pub mod text_service {
-    tonic::include_proto!("textservice");
+pub mod text_svc {
+    pub mod text_service {
+        tonic::include_proto!("textservice");
+    }
+    pub mod user_mention_service {
+        tonic::include_proto!("usermention");
+    }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct TextSvcImpl {
     url_client_pool: Arc<HashMap<&'static str, &'static str>>,
-    user_mention_client_pool: Arc<HashMap<&'static str, &'static str>>,
+    user_mention_client: UserMentionServiceClient<tonic::transport::Channel>,
 }
+
+impl TextSvcImpl {
+    pub fn new(
+        url_client_pool: Arc<HashMap<&'static str, &'static str>>,
+        user_mention_client: UserMentionServiceClient<tonic::transport::Channel>,
+    ) -> Self {
+        TextSvcImpl {
+            url_client_pool,
+            user_mention_client,
+        }
+    }
+}
+
 
 #[tonic::async_trait]
 impl TextService for TextSvcImpl {
@@ -76,37 +96,57 @@ impl TextService for TextSvcImpl {
 
         // async func to get user mention、
         let user_mention_future = {
-            let user_client_pool = &self.user_mention_client_pool;
+            let mut user_mention_client = self.user_mention_client.clone();
             let mention_usernames = &mention_usernames;
             async move {
-                let mut user_mentions = Vec::new();
-                for username in mention_usernames {
-                    match user_client_pool.get(&username as &str) {
-                        Some(user_id) => {
-                            user_mentions.push(user_id.to_string());
+                let user_mention_request = ComposeUserMentionRequest {
+                    req_id: 12345,
+                    usernames: mention_usernames.clone(),
+                };
+                let response = user_mention_client
+                    .compose_user_mentions(Request::new(user_mention_request))
+                    .await;
+                match response {
+                    Ok(res) => {
+                        let inner = res.into_inner();
+                        if let Some(exception) = inner.exception {
+                            return Err(Status::internal(exception.message));
                         }
-                        None => {
-                            error!("User not found for: {}", username);
-                            continue;
-                        }
+                        return Ok(inner.user_mentions);
                     }
-                }
-                user_mentions
+                    Err(status) => {
+                        error!("Error calling user_mention service: {}", status);
+                        return Err(status);
+                    }
+                }           
             }
         };
 
 
         // process the text with url
-        let user_mentions = user_mention_future.await;
         let shortened_urls = shortened_url_future.await;
+        let Ok(user_mentions) = user_mention_future.await else {
+            return Err(Status::internal("Failed to get user mentions"));
+        };
+        // print lenth of user mentions
+        println!("User mentions found: {:?}", user_mentions.len());
 
         let mut updated_text = text.clone();
         for (url, shortened_url) in url_links.iter().zip(shortened_urls.iter()) {
             updated_text = updated_text.replace(url, shortened_url);
         }
 
+        let mut user_mention_id: Vec<String> = Vec::new();
+        for mention in &user_mentions {
+            println!("User mention: {:?}", mention);
+            let username = &mention.username;
+            let user_id = mention.user_id;
+            user_mention_id.push(user_id.to_string());
+            updated_text = updated_text.replace(username, &format!("@{}[{}]", username, user_id));
+        }
+
         let reply = TextReply {
-            user_mentions: user_mentions.into_iter().map(String::from).collect(),
+            user_mentions: user_mention_id,
             urls: shortened_urls.into_iter().map(String::from).collect(),
             updated_text: updated_text.into(),
         };
@@ -116,23 +156,7 @@ impl TextService for TextSvcImpl {
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let url_map = Arc::new(get_url_map());
-    let user_map = Arc::new(get_user_map());
-    let textsvc = TextSvcImpl {
-        url_client_pool: Arc::clone(&url_map),
-        user_mention_client_pool: Arc::clone(&user_map),
-    };
 
-    Server::builder()
-        .add_service(TextServiceServer::new(textsvc))
-        .serve(addr)
-        .await?;
-
-    Ok(())
-}
 
 fn get_url_map() -> HashMap<&'static str, &'static str> {
     let mut map = HashMap::new();
@@ -152,4 +176,10 @@ fn get_user_map() -> HashMap<&'static str, &'static str> {
     map
 }
 
-
+pub async fn create_service() -> TextServiceServer<TextSvcImpl> {
+    let service = TextSvcImpl::new(
+        Arc::new(get_url_map()),
+        UserMentionServiceClient::connect("http://[::1]:50052").await.unwrap(),
+    );
+    TextServiceServer::new(service)
+}
