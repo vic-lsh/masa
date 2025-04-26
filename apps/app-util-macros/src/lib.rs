d13e4d3876987fb9553affef0365a2a5cf0e45e5
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2025-04-26 18:58:07 +0000

    feat: add track latency macro

--- apps/app-util-macros/src/lib.rs
+++ apps/app-util-macros/src/lib.rs
@@ -1,14 +1,60 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// File: latency_tracker/src/lib.rs
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse::Parse, parse::ParseStream, parse_macro_input, Expr, Token};
+
+// Define the structure for parsing macro arguments
+struct TrackLatencyArgs {
+    tracker: Expr,
+    code_block: Expr,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// Implement the Parse trait for our arguments structure
+impl Parse for TrackLatencyArgs {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let tracker = input.parse()?;
+        input.parse::<Token![,]>()?;
+        let code_block = input.parse()?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        Ok(TrackLatencyArgs {
+            tracker,
+            code_block,
+        })
     }
+}
+
+/// Tracks the execution time of a code block and adds it to the provided latency tracker.
+///
+/// # Example
+/// ```
+/// use app_utils::latency::new_latency_tracker;
+/// use app_util_macros::track_latency;
+///
+/// let (mut tracker, _consumer) = new_latency_tracker("code-block-name");
+/// track_latency!(tracker, {
+///     // Code to track
+///     std::thread::sleep(std::time::Duration::from_millis(100));
+/// });
+/// ```
+#[proc_macro]
+pub fn track_latency(input: TokenStream) -> TokenStream {
+    // Parse the input tokens
+    let TrackLatencyArgs {
+        tracker,
+        code_block,
+    } = parse_macro_input!(input as TrackLatencyArgs);
+
+    // Generate the output code
+    let expanded = quote! {
+        {
+            let start = std::time::Instant::now();
+            let result = { #code_block };
+            let duration = start.elapsed();
+            #tracker.track(duration.as_micros().try_into().unwrap());
+            result
+        }
+    };
+
+    // Return the generated code
+    expanded.into()
 }
