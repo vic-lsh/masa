b76c5e802d77904ae571012fff194499b837d13d
aellison <andreas.ellison@gmail.com>
2025-05-20 18:24:26 -0700

    extract logic for logging to file

--- apps/app-utils/src/logging.rs
+++ apps/app-utils/src/logging.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+
 use env_logger::{Builder, Env};
 
 pub fn init_logging() {
@@ -14,6 +16,26 @@ pub fn init_logging() {
                 record.args()
             )
         })
+        .init();
+    log::info!("Logging initialized");
+}
+
+pub fn init_logging_file(output_path: &str) {
+    let file = File::create(format!("{}/client.log", output_path)).expect("couldn't create file");
+
+    Builder::from_env(Env::default().default_filter_or("info"))
+        .format(|buf, record| {
+            use std::io::Write;
+            writeln!(
+                buf,
+                "{} [{}:{}] {}",
+                record.level(),
+                record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
+                record.args()
+            )
+        })
+        .target(env_logger::Target::Pipe(Box::new(file)))
         .init();
     log::info!("Logging initialized");
 }
