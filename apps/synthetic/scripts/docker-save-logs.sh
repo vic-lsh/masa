ea0b5110a24622c1a58052c7fbf1de18a35a0b61
Andreas Ellison <andrelli@cs.washington.edu>
2025-05-23 17:35:03 +0000

    include app config in experiments and fix docker-save-logs.sh

--- apps/synthetic/scripts/docker-save-logs.sh
+++ apps/synthetic/scripts/docker-save-logs.sh
@@ -12,12 +12,12 @@ for ((i=0; i<${#container_names[@]}; i++)); do
   service=${container_names[$i]}
   
   # Get the container ID for the current container
-  container_id=$(docker ps --filter "name=" --format "{{.ID}}")
+  container_id=$(docker ps --filter "name=$service" --format "{{.ID}}")
   
   # Send the command to follow logs
   if [ -n "$container_id" ]; then
     docker logs $container_id &> $1/$service.log
   else
-    echo "Container with name ${container_names[$i]} not found"
+    echo "Container with name ${service} not found"
   fi
 done
