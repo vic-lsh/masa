37168fbe1aeaf7ae1e981ef85d5526bbf3c873f8
Andreas Ellison <andrelli@cs.washington.edu>
2025-05-23 17:35:03 +0000

    include app config in experiments and fix docker-save-logs.sh

--- apps/app-utils/scripts/run_experiment.sh
+++ apps/app-utils/scripts/run_experiment.sh
@@ -9,19 +9,23 @@ fi
 app=$(basename $pwd)
 dir=~/masa/data/$app/$1
 mkdir -p $dir
+mkdir -p /tmp/save/
 # save previous config
 if [[ -f ./scripts/gen_config.json ]]; then
-    cp ./scripts/gen_config.json /tmp/gen_config.json
+    cp ./scripts/gen_config.json /tmp/save/gen_config.json
+fi
+if [[ -f ./scripts/local/config.docker.json ]]; then
+    cp ./scripts/local/config.docker.json /tmp/save/config.docker.json
 fi
 # load config from $1
 cp $dir/gen_config.json ./scripts/gen_config.json
+cp $dir/config.docker.json ./scripts/local/config.docker.json
 
-mkdir -p /tmp/save/
 # save old contents of $1 just in case
 cp -r $dir /tmp/save/
 # clean $dir
 shopt -s extglob
-rm -rf $dir/!(gen_config.json|policies)
+rm -rf $dir/!(gen_config.json|policies|config.docker.json)
 
 policies=$(cat $dir/policies | tr -d '\n')
 
