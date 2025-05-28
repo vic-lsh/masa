0bfa928cc8085cb8ecb77ebfb95b719dc73eb46b
aellison <andreas.ellison@gmail.com>
2025-05-28 10:55:25 -0700

    track handler latencies

--- apps/synthetic/src/child/server.rs
+++ apps/synthetic/src/child/server.rs
@@ -5,7 +5,7 @@ pub mod synthetic_tonic {
 }
 
 use rand_distr::{Distribution, Normal};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use tonic::{Request, Response, Status};
 
@@ -23,6 +23,7 @@ impl ChildImpl {
         ChildImpl {
             constant_latency: config.child_constant_latency,
             random_latency_distribution: Normal::new(
+                // convert from us to ns
                 (config.child_random_mean * 1000) as f64,
                 (config.child_random_std * 1000) as f64,
             )
@@ -31,27 +32,42 @@ impl ChildImpl {
     }
 }
 
+// busy spin duration time
+fn do_work(duration: Duration) {
+    let end = Instant::now() + duration;
+
+    while Instant::now() < end {}
+}
+
 #[tonic::async_trait]
 impl Child for ChildImpl {
     async fn constant_latency(
         &self,
         _request: Request<child::ConstantLatencyRequest>,
     ) -> Result<Response<child::ConstantLatencyResponse>, Status> {
-        tokio::time::sleep(Duration::from_micros(self.constant_latency)).await;
+        let start = Instant::now();
 
-        Ok(Response::new(child::ConstantLatencyResponse {}))
+        do_work(Duration::from_micros(self.constant_latency));
+
+        Ok(Response::new(child::ConstantLatencyResponse {
+            handler_latency: Instant::now().duration_since(start).as_micros() as u64,
+        }))
     }
 
     async fn random_latency(
         &self,
         _request: Request<child::RandomLatencyRequest>,
     ) -> Result<Response<child::RandomLatencyResponse>, Status> {
-        let sleep_duration = Duration::from_nanos(
+        let start = Instant::now();
+
+        let duration = Duration::from_nanos(
             self.random_latency_distribution
                 .sample(&mut rand::thread_rng()) as u64,
         );
-        tokio::time::sleep(sleep_duration).await;
+        do_work(duration);
 
-        Ok(Response::new(child::RandomLatencyResponse {}))
+        Ok(Response::new(child::RandomLatencyResponse {
+            handler_latency: Instant::now().duration_since(start).as_micros() as u64,
+        }))
     }
 }
