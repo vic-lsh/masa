291a1ea2179207da499025890abf26805b60e6a7
aellison <andreas.ellison@gmail.com>
2025-05-29 12:09:35 -0700

    add options to load gen for writing logs to file + formatting

--- apps/hotel/src/recommendation/main.rs
+++ apps/hotel/src/recommendation/main.rs
@@ -10,8 +10,8 @@ use std::path::PathBuf;
 use structopt::StructOpt;
 use tonic::transport::Server;
 
-use config::HotelConfig;
 use app_utils::logging::init_logging;
+use config::HotelConfig;
 use server::hotel_tonic::recommendation::recommendation_server::RecommendationServer;
 use server::RecommendationImpl;
 
