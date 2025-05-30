24cb410cb8371c65a071ed7b0784f921c50460cf
aellison <andreas.ellison@gmail.com>
2025-05-30 10:06:46 -0700

    add plotting scripts and add plotting option to run scripts

---
+++ apps/scripts/plotting/plot-experiment.sh
@@ -0,0 +1,8 @@
+pwd=$(pwd)
+if [[ "$pwd" != */apps/* ]]; then
+    echo "Error: please run in an application directory" >&2
+    exit 1
+fi
+experiment=$1
+
+python3 ../scripts/all.py --config-dir data/in/$1 --data-dir data/out/$1 --output-dir data/plots/$1
--- apps/scripts/queue-experiments.sh
+++ apps/scripts/queue-experiments.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+plot_arg=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    --plot)
+        plot_arg="--plot"
+        shift 1
+        ;;
+    *)
+        echo "Unknown argument: $1"
+        exit 1
+        ;;
+    esac
+done
+
 for exp in $1;
 do
 	echo "running experiment $exp"
-	./scripts/run-experiment.sh $exp
+	./scripts/run-experiment.sh $exp $plot_arg
 done
