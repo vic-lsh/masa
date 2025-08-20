5dd345fa7cf3cd1d2af9a31c08f5fc9b7c7433c8
Andreas Ellison <andrelli@cs.washington.edu>
2025-08-20 12:55:17 -0700

    refactor/fix save-log scripts

--- apps/synthetic/scripts/docker-save-logs.sh
+++ apps/synthetic/scripts/docker-save-logs.sh
@@ -13,15 +13,6 @@ for i in $(seq 1 $CHILD_REPLICAS); do
 done
 
 for ((i=0; i<${#container_names[@]}; i++)); do
-  service=${container_names[$i]}
-  
-  # Get the container ID for the current container
-  container_id=$(docker ps -a --filter "name=$service\$" --format "{{.ID}}")
-  
-  # Send the command to follow logs
-  if [ -n "$container_id" ]; then
-    docker logs $container_id &> $1/$service.log
-  else
-    echo "Container with name ${service} not found"
-  fi
+  name=${container_names[$i]}
+  docker logs $name &> $1/$name.log
 done
