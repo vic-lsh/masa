660bd9338464b00c5edb2338d672c1454cea5bf0
jiexiao <jiexiao@cs.washington.edu>
2025-08-27 00:01:23 +0000

    docker run setup

--- apps/hotel/scripts/local/run_all.sh
+++ apps/hotel/scripts/local/run_all.sh
@@ -130,8 +130,8 @@ build() {
 reset() {
     rm $output_path/*.log
     docker volume prune -a -f
-    docker compose -f scripts/local/containers.yaml down --remove-orphans
-    docker compose -f scripts/local/containers.yaml up -d
+    docker-compose -f scripts/local/containers.yaml down --remove-orphans
+    docker-compose -f scripts/local/containers.yaml up -d
 }
 
 cleanup() {
