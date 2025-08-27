095741b8c8da813e2f2e230807ad9d0081c54a17
jiexiao <jiexiao@cs.washington.edu>
2025-08-27 00:01:23 +0000

    docker run setup

--- apps/hotel/data/lat_out/collect_docker_data.sh
+++ apps/hotel/data/lat_out/collect_docker_data.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Set rps and feature 
+RPS=5000
+FEATURE=prio_global
+
 # change work dir 
 cd ../..
 ./scripts/docker-stop.sh
-./scripts/docker-run.sh --features fifo
-./scripts/loadgen-run.sh
+./scripts/docker-run.sh --features $FEATURE
+./scripts/loadgen-run.sh --output ./data/lat_out/rps$RPS/$FEATURE --save-logs
 
-cd data/lat_out/rps3000/fifo
+cd data/lat_out/rps$RPS/$FEATURE
 
 docker cp hotel_frontend:/HandleReservation_latencies.log .
 docker cp hotel_user:/CheckUser_latencies.log .
