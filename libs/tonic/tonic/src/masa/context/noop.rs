d2911f979a6a6f23d1b96499a9c41a23683724f8
jiexiao <jiexiao@cs.washington.edu>
2025-08-27 22:51:25 +0000

    Modify the fifo context to obtain queue, io, and compute lat

--- libs/tonic/tonic/src/masa/context/noop.rs
+++ libs/tonic/tonic/src/masa/context/noop.rs
@@ -1,7 +1,16 @@
 use std::sync::Arc;
+use std::time::Instant;
 
 use super::{ClientHooks, ParentHooks, PrioritySelector, ServerHooks};
+use crate::masa::context::read_context;
 use crate::{GrpcMethod, Request};
+
+use crate::body::BoxBody;
+use std::sync::atomic::{AtomicU64, AtomicUsize, Ordering};
+use crate::Response;
+use crate::Status;
+
+use masa::{time_now};
 
 #[derive(Debug)]
 pub struct NoopPrioritySelector;
@@ -14,7 +23,16 @@ impl PrioritySelector for NoopPrioritySelector {
 
 #[derive(Debug)]
 #[allow(dead_code)]
-pub struct ParentContext {}
+pub struct ParentContext {
+    method: GrpcMethod,
+    ctx: masa::Context,
+
+    num_polled: AtomicUsize,
+    start_exec: Instant,
+    last_before_poll: AtomicU64,
+    compute_latency: AtomicU64,
+
+}
 
 #[derive(Debug, Clone)]
 pub struct ChildContext {}
@@ -29,7 +47,47 @@ impl ParentHooks<ChildContext, ServerContext> for ParentContext {
         _req: &http::Request<B>,
         _server_ctx: Arc<ServerContext>,
     ) -> Self {
-        Self {}
+        Self {
+            method: _method,
+            ctx: read_context(_req),
+            num_polled: AtomicUsize::new(0),
+            start_exec: Instant::now(),
+            last_before_poll: AtomicU64::new(0),
+            compute_latency: AtomicU64::new(0),
+        }
+    }
+
+    fn before_poll<Ret>(&self) -> Result<(), Result<Response<Ret>, Status>> {
+        let now = time_now();
+        self.last_before_poll.store(now, Ordering::Release);
+        
+        Ok(())
+    }
+
+    fn after_poll<Ret>(
+            &self,
+            poll: &std::task::Poll<Result<Response<Ret>, Status>>,
+        ) -> Result<(), Result<Response<Ret>, Status>> {
+        let now = time_now();
+        let last_before_poll = self.last_before_poll.load(Ordering::Acquire);
+        assert!(last_before_poll != 0);
+        let compute_latency = now - last_before_poll;
+        self.compute_latency.fetch_add(compute_latency, Ordering::AcqRel);
+
+        Ok(())
+    }
+
+    fn finalize(&self, _response: &mut http::Response<BoxBody>) {
+        // read the timer from the header
+        let e2e_latency = self.start_exec.elapsed().as_micros() as u64;
+        let compute_latency = self.compute_latency.load(Ordering::Acquire);
+        // obtain the queue lat from header
+        let queue_latency = 0;
+        let io_latency = e2e_latency.saturating_sub(compute_latency).saturating_sub(queue_latency);
+
+        // Get the task of the request
+        log::info!("Request {:?} completed. E2E latency: {}, Compute latency: {}, IO latency: {}, Queue latency: {}",
+            self.method.id(), e2e_latency, compute_latency, io_latency, queue_latency);
     }
 }
 
