00072f7a0eb982a78a9ec592c9c9d8feec61f890
vic-lsh <shli@cs.washington.edu>
2025-09-01 15:24:54 -0700

    debug: check output

--- scripts/test_e2e_hotel.sh
+++ scripts/test_e2e_hotel.sh
@@ -18,7 +18,7 @@ policy_config=$ci_config_path/policies
 # patch experiment settings
 
 jq '.Rps = [1000]' $gen_config > tmp.json && mv tmp.json $gen_config
-echo "fifo prio_global prio_local" > $policy_config
+echo "fifo" > $policy_config
 
 # run experiment
 
@@ -26,18 +26,19 @@ echo "fifo prio_global prio_local" > $policy_config
 
 assert_file_exists() {
     if [ -f "$1" ]; then
-        ls -la "data/out"
         echo "Error: File '$1' not found." >&2; exit 1;
     else
         echo "Assertion passed: File '$1' exists"
     fi
 }
 
+ls -la "data/out"
+ls -la "data/out/0"
 # assert result files exist
 assert_file_exists "data/out/$exp_name/done"
 assert_file_exists "data/out/$exp_name/0/fifo/r1000_Reservation.csv"
 assert_file_exists "data/out/$exp_name/0/fifo/r1000_Search.csv"
-assert_file_exists "data/out/$exp_name/0/prio_local/r1000_Reservation.csv"
-assert_file_exists "data/out/$exp_name/0/prio_local/r1000_Search.csv"
-assert_file_exists "data/out/$exp_name/0/prio_global/r1000_Reservation.csv"
-assert_file_exists "data/out/$exp_name/0/prio_global/r1000_Search.csv"
+# assert_file_exists "data/out/$exp_name/0/prio_local/r1000_Reservation.csv"
+# assert_file_exists "data/out/$exp_name/0/prio_local/r1000_Search.csv"
+# assert_file_exists "data/out/$exp_name/0/prio_global/r1000_Reservation.csv"
+# assert_file_exists "data/out/$exp_name/0/prio_global/r1000_Search.csv"
