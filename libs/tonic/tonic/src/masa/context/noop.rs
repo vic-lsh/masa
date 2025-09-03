2a71ef62de4b1f0bdf926af068bbf69d0578e314
vic-lsh <shli@cs.washington.edu>
2025-09-02 17:42:26 -0700

    refactor: move tracing hook into a new module

--- libs/tonic/tonic/src/masa/context/noop.rs
+++ libs/tonic/tonic/src/masa/context/noop.rs
@@ -1,16 +1,7 @@
 use std::sync::Arc;
-use std::time::Instant;
 
 use super::{ClientHooks, ParentHooks, PrioritySelector, ServerHooks};
-use crate::masa::context::read_context;
 use crate::{GrpcMethod, Request};
-
-use crate::body::BoxBody;
-use crate::Response;
-use crate::Status;
-use std::sync::atomic::{AtomicU64, AtomicUsize, Ordering};
-
-use masa::time_now;
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -43,51 +34,7 @@ impl ParentHooks<ChildContext, ServerContext> for ParentContext {
         _req: &http::Request<B>,
         _server_ctx: Arc<ServerContext>,
     ) -> Self {
-        Self {
-            method: _method,
-            num_polled: AtomicUsize::new(0),
-            start_exec: Instant::now(),
-            last_before_poll: AtomicU64::new(0),
-            compute_latency: AtomicU64::new(0),
-        }
-    }
-
-    fn before_poll<Ret>(&self) -> Result<(), Result<Response<Ret>, Status>> {
-        self.last_before_poll.store(time_now(), Ordering::Release);
-
-        Ok(())
-    }
-
-    fn after_poll<Ret>(
-        &self,
-        _poll: &std::task::Poll<Result<Response<Ret>, Status>>,
-    ) -> Result<(), Result<Response<Ret>, Status>> {
-        let last_before_poll = self.last_before_poll.load(Ordering::Acquire);
-        assert!(last_before_poll != 0);
-        let compute_latency = time_now() - last_before_poll;
-        self.compute_latency
-            .fetch_add(compute_latency, Ordering::AcqRel);
-        Ok(())
-    }
-
-    fn finalize(&self, _response: &mut http::Response<BoxBody>) {
-        // read the timer from the header
-        let e2e_latency = self.start_exec.elapsed().as_micros() as u64;
-        let compute_latency = self.compute_latency.load(Ordering::Acquire);
-        // obtain the queue lat from header
-        let queue_latency = tokio::task::obtain_task_queue_latency().as_micros() as u64;
-        let io_latency = e2e_latency
-            .saturating_sub(compute_latency)
-            .saturating_sub(queue_latency);
-
-        let latency_str = format!(
-            "{},{},{},{}",
-            e2e_latency, compute_latency, io_latency, queue_latency
-        );
-        // insert the latency info to header
-        let res_header = _response.headers_mut();
-        let header_val = http::HeaderValue::from_str(&latency_str).unwrap();
-        res_header.insert("X-Request-Latency", header_val);
+        Self {}
     }
 }
 
