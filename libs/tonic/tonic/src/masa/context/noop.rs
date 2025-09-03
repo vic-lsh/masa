4199dd3da41fbf66a5cdc9ce4a9841ea9b5f48bf
vic-lsh <shli@cs.washington.edu>
2025-09-02 17:20:25 -0700

    chore: format

--- libs/tonic/tonic/src/masa/context/noop.rs
+++ libs/tonic/tonic/src/masa/context/noop.rs
@@ -6,11 +6,11 @@ use crate::masa::context::read_context;
 use crate::{GrpcMethod, Request};
 
 use crate::body::BoxBody;
-use std::sync::atomic::{AtomicU64, AtomicUsize, Ordering};
 use crate::Response;
 use crate::Status;
+use std::sync::atomic::{AtomicU64, AtomicUsize, Ordering};
 
-use masa::{time_now};
+use masa::time_now;
 
 #[derive(Debug)]
 pub struct NoopPrioritySelector;
@@ -30,7 +30,6 @@ pub struct ParentContext {
     start_exec: Instant,
     last_before_poll: AtomicU64,
     compute_latency: AtomicU64,
-
 }
 
 #[derive(Debug, Clone)]
@@ -57,18 +56,19 @@ impl ParentHooks<ChildContext, ServerContext> for ParentContext {
 
     fn before_poll<Ret>(&self) -> Result<(), Result<Response<Ret>, Status>> {
         self.last_before_poll.store(time_now(), Ordering::Release);
-        
+
         Ok(())
     }
 
     fn after_poll<Ret>(
-            &self,
-            poll: &std::task::Poll<Result<Response<Ret>, Status>>,
-        ) -> Result<(), Result<Response<Ret>, Status>> {
+        &self,
+        _poll: &std::task::Poll<Result<Response<Ret>, Status>>,
+    ) -> Result<(), Result<Response<Ret>, Status>> {
         let last_before_poll = self.last_before_poll.load(Ordering::Acquire);
         assert!(last_before_poll != 0);
         let compute_latency = time_now() - last_before_poll;
-        self.compute_latency.fetch_add(compute_latency, Ordering::AcqRel);
+        self.compute_latency
+            .fetch_add(compute_latency, Ordering::AcqRel);
         Ok(())
     }
 
@@ -78,9 +78,14 @@ impl ParentHooks<ChildContext, ServerContext> for ParentContext {
         let compute_latency = self.compute_latency.load(Ordering::Acquire);
         // obtain the queue lat from header
         let queue_latency = tokio::task::obtain_task_queue_latency().as_micros() as u64;
-        let io_latency = e2e_latency.saturating_sub(compute_latency).saturating_sub(queue_latency);
+        let io_latency = e2e_latency
+            .saturating_sub(compute_latency)
+            .saturating_sub(queue_latency);
 
-        let latency_str = format!("{},{},{},{}", e2e_latency, compute_latency, io_latency, queue_latency);
+        let latency_str = format!(
+            "{},{},{},{}",
+            e2e_latency, compute_latency, io_latency, queue_latency
+        );
         // insert the latency info to header
         let res_header = _response.headers_mut();
         let header_val = http::HeaderValue::from_str(&latency_str).unwrap();
