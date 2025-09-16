6d27d9d5a5bf53bf176c3411cef9f98f46be87e3
jiexiao <jiexiao@cs.washington.edu>
2025-09-16 22:04:23 +0000

    Add Ver01 Offline Optimal Scheduler

--- libs/tokio/tokio/src/runtime/task/core.rs
+++ libs/tokio/tokio/src/runtime/task/core.rs
@@ -211,8 +211,7 @@ impl TraceTimer {
     }
 
     pub(crate) fn record_queue_lat(&mut self) {
-        let latency = Instant::now().duration_since(self.last_enqueue.unwrap());
-        self.q_lat = latency;
+        self.q_lat = Instant::now().duration_since(self.last_enqueue.unwrap());
     }
 
     pub(crate) fn q_lat(&self) -> Duration {
