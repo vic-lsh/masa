c2d98ea6c3c650b07c6307a79f1dde02b6f517d3
vic-lsh <shli@cs.washington.edu>
2025-09-20 17:28:25 -0700

    feat(socialnet): impl first version of user_timeline server

--- apps/socialnet/build.rs
+++ apps/socialnet/build.rs
@@ -4,5 +4,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("proto/url_shorten.proto")?;
     tonic_build::compile_protos("proto/usermention.proto")?;
     tonic_build::compile_protos("proto/user.proto")?;
+    tonic_build::compile_protos("proto/user_timeline.proto")?;
+    tonic_build::compile_protos("proto/post_storage.proto")?;
     Ok(())
 }
