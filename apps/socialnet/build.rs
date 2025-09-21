81d406fcce0e696f68ae400ceb31d9a3f5163cee
vic-lsh <shli@cs.washington.edu>
2025-09-20 17:07:55 -0700

    feat(socialnet): impl user service

--- apps/socialnet/build.rs
+++ apps/socialnet/build.rs
@@ -3,5 +3,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("proto/media.proto")?;
     tonic_build::compile_protos("proto/url_shorten.proto")?;
     tonic_build::compile_protos("proto/usermention.proto")?;
+    tonic_build::compile_protos("proto/user.proto")?;
     Ok(())
 }
---
+++ apps/socialnet/src/user/main.rs
@@ -0,0 +1,6 @@
+mod server;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    todo!()
+}
