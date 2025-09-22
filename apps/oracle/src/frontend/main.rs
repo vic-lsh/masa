eb636190fa96ca122b3882b1141c752eb7ce1ae9
jiexiao <jiexiao@cs.washington.edu>
2025-09-22 20:56:37 +0000

    WIP: Add oracle app (copy from the synthetic dataset) for the online scheduler, and apply the formatter

---
+++ apps/oracle/src/frontend/main.rs
@@ -0,0 +1,47 @@
+mod server;
+
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+use tonic::transport::Server;
+
+use synthetic_app::config;
+
+use app_utils::logging::init_logging;
+use config::SyntheticConfig;
+use server::FrontendImpl;
+use synthetic_app::tonic::frontend::frontend_server::FrontendServer;
+
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(about = "Synthetic Args")]
+pub struct Args {
+    #[structopt(short, long, required = true)]
+    pub config: PathBuf,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    init_logging();
+
+    let args = Args::from_args();
+    let cfg: SyntheticConfig = {
+        let file = File::open(args.config).expect("Failed to open file");
+        let reader = BufReader::new(file);
+        serde_json::from_reader(reader)?
+    };
+
+    const PORT: u16 = 8000;
+    let frontend_addr = format!("{}:{}", "[::]", PORT)
+        .parse()
+        .expect("Failed to parse address");
+    let frontend = FrontendImpl::new(cfg).await;
+    log::info!("Server listening on {}...", frontend_addr);
+    Server::builder()
+        .add_service(FrontendServer::new(frontend))
+        .serve_with_masa(frontend_addr)
+        .await?;
+
+    Ok(())
+}
