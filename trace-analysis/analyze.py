aa0e50a6ef59144962fb60f8ad261087ab688c8f
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2025-09-22 05:31:36 +0000

    fix(trace): import order

--- trace-analysis/analyze.py
+++ trace-analysis/analyze.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
-from networkx.classes.digraph import DiGraph
 from __future__ import annotations
+
+from networkx.classes.digraph import DiGraph
+
 
 # Headless/parallel-safe plotting
 import matplotlib
