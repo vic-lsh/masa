da3857c1ef3139e003f574ca5499ad1ce4db150f
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2025-09-23 18:54:48 +0000

    impl(mssim): rm dead code

--- apps/mssim/simulator/src/client/cli.rs
+++ apps/mssim/simulator/src/client/cli.rs
@@ -11,10 +11,6 @@ pub struct CliOptions {
     /// Path to the input JSON file
     pub alibaba_trace: Option<PathBuf>,
 
-    #[structopt(short, long, parse(from_os_str))]
-    /// Path to the input JSON file
-    pub input: Option<PathBuf>,
-
     #[structopt(short, long, default_value = "localhost:50051")]
     /// Address of the orchestrator service
     pub orchestrator: String,
