d092c1e1876b281901ef513288193f67bc734184
jiexiao <jiexiao@cs.washington.edu>
2025-09-23 18:52:16 +0000

    Infra for the online scheduler

--- apps/synthetic/src/client/client_bench.rs
+++ apps/synthetic/src/client/client_bench.rs
@@ -2,6 +2,7 @@ pub mod frontend {
     tonic::include_proto!("frontend");
 }
 use std::path::Path;
+use std::sync::Arc;
 
 use rand::rngs::StdRng;
 use structopt::StructOpt;
@@ -47,6 +48,7 @@ const PRESAMPLED_PREFIX: &'static str = "presampled_";
 enum RequestHandler {
     ARequest(Handler<ARequest, SyntheticClient>),
     PresampledRequest(Handler<PresampledRequest, SyntheticClient>),
+    TracedRequest(Handler<TracedRequest, SyntheticClient>),
 }
 
 impl HandlerOuter<SyntheticClient> for RequestHandler {
@@ -70,6 +72,7 @@ impl HandlerOuter<SyntheticClient> for RequestHandler {
         match self {
             Self::ARequest(h) => h.send_request(rng, client, ctx, trace).await,
             Self::PresampledRequest(h) => h.send_request(rng, client, ctx, trace).await,
+            Self::TracedRequest(h) => h.send_request(rng, client, ctx, trace).await,
         }
     }
 
@@ -77,6 +80,7 @@ impl HandlerOuter<SyntheticClient> for RequestHandler {
         match self {
             Self::ARequest(h) => h.fetch_traces(output_path).await,
             Self::PresampledRequest(h) => h.fetch_traces(output_path).await,
+            Self::TracedRequest(h) => h.fetch_traces(output_path).await,
         }
     }
 
@@ -84,6 +88,7 @@ impl HandlerOuter<SyntheticClient> for RequestHandler {
         match self {
             Self::ARequest(h) => h.api.as_str(),
             Self::PresampledRequest(h) => h.api.as_str(),
+            Self::TracedRequest(h) => h.api.as_str(),
         }
     }
 
@@ -91,6 +96,7 @@ impl HandlerOuter<SyntheticClient> for RequestHandler {
         match self {
             Self::ARequest(h) => h.slo,
             Self::PresampledRequest(h) => h.slo,
+            Self::TracedRequest(h) => h.slo,
         }
     }
 }
@@ -171,6 +177,37 @@ impl RequestType<SyntheticClient> for PresampledRequest {
         });
         r.metadata_mut().insert_ctx("ctx", &ctx);
         client.handle_presampled(r).await
+    }
+
+    fn response_output_headers(&self) -> Vec<String> {
+        Vec::new()
+    }
+
+    fn response_to_row(_metadata: &MetadataMap, _r: &Self::ResponseType) -> Vec<String> {
+        Vec::new()
+    }
+}
+
+struct TracedRequest {
+    req_id: u64,
+}
+
+impl RequestType<SyntheticClient> for TracedRequest {
+    type ResponseType = frontend::TracedResponse;
+
+    fn new(req_id: u64) -> Self {
+        Self { req_id }
+    }
+
+    async fn create_request(
+        &self,
+        _rng: &mut StdRng,
+        mut client: <SyntheticClient as Client>::FrontendClient,
+        ctx: &Context,
+    ) -> Result<Response<Self::ResponseType>, Status> {
+        let mut r = tonic::Request::new(frontend::TracedRequest { req_id: self.req_id });
+        r.metadata_mut().insert_ctx("ctx", &ctx);
+        client.handle_traced(r).await
     }
 
     fn response_output_headers(&self) -> Vec<String> {
