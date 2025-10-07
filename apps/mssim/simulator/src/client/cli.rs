3cce26d4fbfde9e712869a04035132923f522163
jiexiao <jiexiao@cs.washington.edu>
2025-10-07 18:05:33 +0000

    Add goodput time series, and add replay path to the alibaba trace cli instead of hardcoding

--- apps/mssim/simulator/src/client/cli.rs
+++ apps/mssim/simulator/src/client/cli.rs
@@ -11,6 +11,10 @@ pub struct CliOptions {
     /// Path to the input JSON file
     pub alibaba_trace: Option<PathBuf>,
 
+    #[structopt(long, parse(from_os_str))]
+    /// Override the replay trace file used by the load generator
+    pub replay_path: Option<PathBuf>,
+
     #[structopt(short, long, default_value = "localhost:50051")]
     /// Address of the orchestrator service
     pub orchestrator: String,
