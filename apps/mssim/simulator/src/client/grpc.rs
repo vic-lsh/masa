6990d653a1b780732b32c15e11c13d0ce6bdeb20
vic-lsh <shli@cs.washington.edu>
2025-10-11 20:36:49 -0700

    chore(mssim): dead code

--- apps/mssim/simulator/src/client/grpc.rs
+++ apps/mssim/simulator/src/client/grpc.rs
@@ -2,9 +2,10 @@ use anyhow::Result;
 use tonic::{Request, Status};
 
 use crate::proto::{
-    ConfigurationRequest, simulator_orchestrator_client::SimulatorOrchestratorClient,
+    simulator_orchestrator_client::SimulatorOrchestratorClient, ConfigurationRequest,
 };
 
+#[allow(dead_code)]
 pub async fn submit_config_to_orchestrator(
     orchestrator_addr: &str,
     yaml_config: String,
