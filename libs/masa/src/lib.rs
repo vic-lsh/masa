b7a12c16058a0b529272882c6c2ad25d42ec98fb
jiexiao <jiexiao@cs.washington.edu>
2025-10-13 22:07:31 +0000

    Add tracing features

--- libs/masa/src/lib.rs
+++ libs/masa/src/lib.rs
@@ -13,7 +13,7 @@ pub use context::FutureSpan;
 pub use distribution::Distribution;
 pub use flag::{
     FIFO, FIFO_EARLY, FIFO_INFRA, PRIO_CLASS, PRIO_CLASS_GLOBAL, PRIO_GLOBAL, PRIO_GLOBAL_EARLY,
-    PRIO_LOCAL, PRIO_LOCAL_EARLY,
+    PRIO_LOCAL, PRIO_LOCAL_EARLY, PRIO_GLOBAL_QUEUE_TRACING, FIFO_QUEUE_TRACING, FIFO_SPAN_TRACING,
 };
 pub use graph::{
     FutureGraphTracker, GlobalGraph, GlobalGraphTracker, LocalGraph, LocalGraphTracker,
