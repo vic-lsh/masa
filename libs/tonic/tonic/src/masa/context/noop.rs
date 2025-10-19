e41694e4af8efa31e5be2a8e573626791d8254bb
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2025-10-19 17:45:42 +0000

    feat(masa): add back early return impl to fifo and prio_global

--- libs/tonic/tonic/src/masa/context/noop.rs
+++ libs/tonic/tonic/src/masa/context/noop.rs
@@ -1,7 +1,10 @@
-use std::sync::Arc;
+use std::{sync::{atomic::{AtomicBool, Ordering}, Arc}, task::Poll};
+
+use masa::{time_now, Context};
+use tracing::error;
 
 use super::{ClientHooks, ParentHooks, PrioritySelector, ServerHooks};
-use crate::{GrpcMethod, Request};
+use crate::{masa::context::read_context, Code, GrpcMethod, Request, Response, Status};
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -17,7 +20,10 @@ impl PrioritySelector for NoopPrioritySelector {
 #[derive(Debug)]
 #[allow(dead_code)]
 #[allow(unreachable_pub)]
-pub struct ParentContext {}
+pub struct ParentContext {
+    ctx: Context,
+    will_early_return: AtomicBool,
+}
 
 #[derive(Debug, Clone)]
 #[allow(unreachable_pub)]
@@ -28,14 +34,110 @@ pub struct ChildContext {}
 #[allow(unreachable_pub)]
 pub struct ServerContext {}
 
+impl ParentContext {
+    #[inline]
+    fn check_early_return(&self) -> bool {
+        if self.will_early_return.load(Ordering::Relaxed) {
+            return true;
+        }
+
+        let now = time_now();
+        let should_early_return = now >= self.ctx.deadline();
+
+        if should_early_return {
+            // `check_early_return` may be invoked at multiple lifecycle hooks.
+            //
+            // this will only be read/written on one thread, so we can use the
+            // weakest ordering guarantees.
+            // it is an atomic because the ParentContext type needs to be Sync:
+            // see the docs for ParentHooks for why.
+            if self
+                .will_early_return
+                .compare_exchange_weak(false, true, Ordering::Relaxed, Ordering::Relaxed)
+                .is_ok()
+            {
+                error!("Request going to early return");
+                // self.server_ctx
+                //     .num_early_returns
+                //     .fetch_add(1, Ordering::Relaxed);
+            }
+        }
+
+        return should_early_return;
+    }
+
+
+    #[inline]
+    fn issue_early_return(&self) -> Status {
+        Status::new(
+            Code::DeadlineExceeded,
+            format!("/EarlyReturn"),
+        )
+    }
+}
+
 impl ParentHooks<ChildContext, ServerContext> for ParentContext {
     fn begin<B>(
         _method: GrpcMethod,
-        _req: &http::Request<B>,
+        req: &http::Request<B>,
         _server_ctx: Arc<ServerContext>,
     ) -> Self {
-        Self {}
+        Self {
+            ctx: read_context(req),
+            will_early_return: AtomicBool::new(false),
+        }
     }
+
+    fn before_poll<Ret>(&self) -> Result<(), Result<Response<Ret>, Status>> {
+        if self.check_early_return() {
+            return Err(Err(self.issue_early_return()));
+        }
+
+        Ok(())
+    }
+
+    fn before_child_rpc<T>(
+        &self,
+        _child_method: GrpcMethod,
+        request: &mut Request<T>,
+        _child_ctx: &mut ChildContext,
+    ) -> Result<(), Status> {
+        if self.check_early_return() {
+            return Err(self.issue_early_return());
+        }
+
+        let deadline = self.ctx.deadline();
+
+        let child_recv_ctx = Context::new(
+            self.ctx.api().clone(),
+            self.ctx.test_id(),
+            self.ctx.request_id(),
+            self.ctx.slo(),
+            self.ctx.request_class(),
+            self.ctx.start_at(),
+            deadline,
+        );
+        request.metadata_mut().insert_ctx("ctx", &child_recv_ctx);
+
+        Ok(())
+    }
+
+    fn after_poll<Ret>(
+        &self,
+        poll: &Poll<Result<Response<Ret>, Status>>,
+    ) -> Result<(), Result<Response<Ret>, Status>> {
+        match poll {
+            Poll::Pending => {
+                if self.check_early_return() {
+                    return Err(Err(self.issue_early_return()));
+                }
+            }
+            Poll::Ready(_) => {}
+        };
+
+        Ok(())
+    }
+
 }
 
 impl ClientHooks for ChildContext {
