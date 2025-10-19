5f475b9f5ec821be4b26101666e93afcf707ec1b
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2025-10-19 23:32:52 +0000

    chore: rm prio_class_global

--- libs/tokio/tokio/src/runtime/scheduler/current_thread/queue/mod.rs
+++ libs/tokio/tokio/src/runtime/scheduler/current_thread/queue/mod.rs
@@ -10,7 +10,6 @@ pub(crate) type LocalRunQueue<T> = LocalRunQueueInner<T>;
 #[cfg(not(any(
     feature = "prio_class",
     feature = "prio_global",
-    feature = "prio_class_global",
     feature = "prio_local",
     feature = "perfect_lsf",
     feature = "fifo_infra",
