ecfe9f9ed49cb2521e9485de255602def9e28d78
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2025-10-19 17:46:47 +0000

    chore(masa): update selector impl in prio_global

--- libs/tonic/tonic/src/masa/context/mod.rs
+++ libs/tonic/tonic/src/masa/context/mod.rs
@@ -46,8 +46,8 @@ pub type DefaultPrioritySelector = simple::SimplePrioritySelector;
 
 #[cfg(any(feature = "prio_global", feature = "prio_global_early",))]
 #[allow(missing_docs)]
-// pub type DefaultPrioritySelector = queue_global::QueueGlobal;
-pub type DefaultPrioritySelector = global::Global;
+pub type DefaultPrioritySelector = queue_global::QueueGlobal;
+// pub type DefaultPrioritySelector = global::Global;
 
 #[cfg(any(feature = "prio_global_queue_tracing"))]
 #[allow(missing_docs)]
