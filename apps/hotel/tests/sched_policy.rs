a3b08fe81f0595cbdd3d46ec3bfd6cb6f5dabbd3
jiexiao <jiexiao@cs.washington.edu>
2025-10-27 22:18:06 +0000

    Pass test sched policy and format

--- apps/hotel/tests/sched_policy.rs
+++ apps/hotel/tests/sched_policy.rs
@@ -1,4 +1,10 @@
-#[cfg(not(any(feature = "fifo", feature = "prio_global", feature = "prio_local",)))]
+#[cfg(not(any(
+    feature = "fifo",
+    feature = "prio_global",
+    feature = "prio_local",
+    feature = "prio_local_direct",
+    feature = "prio_local_indirect"
+)))]
 #[test]
 fn test_default_policy_is_fifo() {
     assert_eq!(tokio::get_sched_flavor(), tokio::SchedFlavor::Fifo);
@@ -19,5 +25,17 @@ fn test_prio_global_policy() {
 #[cfg(feature = "prio_local")]
 #[test]
 fn test_prio_local_policy() {
+    assert_eq!(tokio::get_sched_flavor(), tokio::SchedFlavor::Prio);
+}
+
+#[cfg(feature = "prio_local_direct")]
+#[test]
+fn test_prio_local_direct_policy() {
+    assert_eq!(tokio::get_sched_flavor(), tokio::SchedFlavor::Prio);
+}
+
+#[cfg(feature = "prio_local_indirect")]
+#[test]
+fn test_prio_local_indirect_policy() {
     assert_eq!(tokio::get_sched_flavor(), tokio::SchedFlavor::Prio);
 }
