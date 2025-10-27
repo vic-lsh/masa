85b308938b7b3417595e08216400f28ad86e03a5
jiexiao <jiexiao@cs.washington.edu>
2025-10-27 21:43:34 +0000

    Add warm-up in the mssim experiment

--- apps/mssim/generic-service/src/replay.rs
+++ apps/mssim/generic-service/src/replay.rs
@@ -8,23 +8,23 @@ use std::{
 };
 
 use anyhow::Context;
-use masa::{time_now, Context as MasaContext};
+use masa::{Context as MasaContext, time_now};
 use serde::Deserialize;
 use serde_json::Value;
 use tokio::fs;
-use tokio::sync::{mpsc, Mutex, Semaphore};
+use tokio::sync::{Mutex, Semaphore, mpsc};
 use tokio::time::Instant;
+use tonic::Request;
 use tonic::metadata::MetadataMap;
 use tonic::transport::masa_channel::LoadBalancedChannel;
-use tonic::Request;
 
 use crate::service::local_span::SpanType as ProtoSpanType;
 use crate::service::service_client::ServiceClient;
 use crate::service::{
-    span::Kind as ProtoSpanKind, ChildSpans as ProtoChildSpans, LocalSpan as ProtoLocalSpan,
-    ReplayRequest as ProtoReplayRequest, Span as ProtoSpan,
+    ChildSpans as ProtoChildSpans, LocalSpan as ProtoLocalSpan,
+    ReplayRequest as ProtoReplayRequest, Span as ProtoSpan, span::Kind as ProtoSpanKind,
 };
-use crate::{Stats, OUTPUT_DIR};
+use crate::{OUTPUT_DIR, Stats};
 
 #[allow(dead_code)]
 #[derive(Debug, Deserialize)]
