bc30e0637fe4823916131ee3424537f0d07eea1b
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2025-12-29 01:58:31 +0000

    refactor: latency distribution trait

--- libs/tonic/tonic/src/masa/context/local/mod.rs
+++ libs/tonic/tonic/src/masa/context/local/mod.rs
@@ -9,14 +9,13 @@ pub(crate) use local_direct::LocalDeadlineDirect;
 #[allow(unused_imports)]
 pub(crate) use local_indirect::LocalDeadlineIndirect;
 
-use masa::LatencyDistribution;
+use masa::LatencyEstimator;
 
 // TODO: tweak these values. should they be specific to each local priority selector?
-const DISTRIBUTION_CAPACITY: usize = 512;
 pub(crate) const PERCENTILE: usize = 50;
 
-fn estimate_method_latency(
-    map: &RwLock<HashMap<String, LatencyDistribution>>,
+fn estimate_method_latency<E: LatencyEstimator + Default + 'static>(
+    map: &RwLock<HashMap<String, E>>,
     key: String,
 ) -> Option<u64> {
     let has_method = {
@@ -34,15 +33,15 @@ fn estimate_method_latency(
     if !has_method {
         map.write().unwrap().insert(
             key.clone(),
-            LatencyDistribution::new(key, DISTRIBUTION_CAPACITY),
+            E::default(),
         );
     }
     None
 }
 
 // TODO: could reduce lock contention by giving each key it's own lock
-fn track_method_latency(
-    map: &RwLock<HashMap<String, LatencyDistribution>>,
+fn track_method_latency<E: LatencyEstimator>(
+    map: &RwLock<HashMap<String, E>>,
     key: String,
     duration: u64,
 ) {
