9f5503c7723823f73bbb9febc38ca2e49678bf90
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2026-01-02 22:13:31 +0000

    refactor: unify MethodId impl in masa

--- libs/tonic/tonic/src/masa/context/local/local_direct.rs
+++ libs/tonic/tonic/src/masa/context/local/local_direct.rs
@@ -208,7 +208,7 @@ impl<E: LatencyEstimator + Default + 'static> ParentHooks<ChildContext, ServerCo
         self.child_end_times
             .lock()
             .unwrap()
-            .push((child_method.id(), Instant::now()));
+            .push((child_method.id().into(), Instant::now()));
 
         if let Ok(resp) = response {
             if let Some(value) = resp
