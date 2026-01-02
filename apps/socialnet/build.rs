ed59d397c4aefac6a85b9de95080274f7213cf6f
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2026-01-02 01:19:03 +0000

    build: use vendored protoc across all apps

--- apps/socialnet/build.rs
+++ apps/socialnet/build.rs
@@ -1,4 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Use vendored protoc to avoid system dependency issues
+    std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
+    
     tonic_build::compile_protos("proto/textservice.proto")?;
     tonic_build::compile_protos("proto/media.proto")?;
     tonic_build::compile_protos("proto/url_shorten.proto")?;
