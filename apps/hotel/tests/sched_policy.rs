8c311ee1a96f06e7bd41034423d35f362ff56667
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2026-01-05 00:02:19 +0000

    chore: fmt

--- apps/hotel/tests/sched_policy.rs
+++ apps/hotel/tests/sched_policy.rs
@@ -1,8 +1,4 @@
-#[cfg(not(any(
-    feature = "fifo",
-    feature = "prio_global",
-    feature = "prio_local"
-)))]
+#[cfg(not(any(feature = "fifo", feature = "prio_global", feature = "prio_local")))]
 #[test]
 fn test_default_policy_is_fifo() {
     assert_eq!(tokio::get_sched_flavor(), tokio::SchedFlavor::Fifo);
