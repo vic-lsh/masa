fe29a88e420aef93d6a5ddffaae26700a911e269
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2026-01-14 02:52:22 +0000

    chore: fmt

--- apps/synthetic/src/client/client_bench.rs
+++ apps/synthetic/src/client/client_bench.rs
@@ -36,7 +36,14 @@ impl Client for SyntheticClient {
             let start_at = time_now();
             let deadline = start_at + slo;
             let req_id = 0;
-            Context::new("ping".to_string(), req_id, slo, start_at, deadline, deadline)
+            Context::new(
+                "ping".to_string(),
+                req_id,
+                slo,
+                start_at,
+                deadline,
+                deadline,
+            )
         };
         request.metadata_mut().insert_ctx("ctx", &ctx);
         client.handle_ping(request).await.map(|_| ())
