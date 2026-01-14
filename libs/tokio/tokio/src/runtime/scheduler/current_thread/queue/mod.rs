5586c4e48222a3bea2cc08ca75fd66c459dcbdf9
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2026-01-14 07:07:56 +0000

    feat: experiment with infra queue

--- libs/tokio/tokio/src/runtime/scheduler/current_thread/queue/mod.rs
+++ libs/tokio/tokio/src/runtime/scheduler/current_thread/queue/mod.rs
@@ -126,6 +126,8 @@ pub(crate) trait Queue {
     fn with_capacity(cap: usize) -> Self;
 }
 
+pub(crate) const USE_INFRA_QUEUE: bool = true;
+
 #[derive(Debug)]
 #[allow(dead_code)]
 pub(crate) enum PushError<T> {
