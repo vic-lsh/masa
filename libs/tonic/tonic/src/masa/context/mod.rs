a5b0d1ea2370d4551370d9d321506a76acffb6b9
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2026-01-14 04:31:53 +0000

    feat: add prio_oldest feature and integrate into context handling
    - Introduced a new feature `prio_oldest` in the Cargo.toml files for relevant apps and libraries.
    - Updated context handling in load generation and replay logic to utilize `prio_hint` based on the `prio_oldest` feature.
    - Implemented `PrioOldest` policy in the masa context to prioritize requests based on their generation time.
    - Adjusted dependencies in various Cargo.toml files to include the new feature where necessary.

--- libs/tonic/tonic/src/masa/context/mod.rs
+++ libs/tonic/tonic/src/masa/context/mod.rs
@@ -7,6 +7,7 @@ mod fifo;
 mod global;
 mod local;
 mod noop;
+mod prio_oldest;
 mod queue_global;
 mod queue_tracing;
 mod tracing;
@@ -16,33 +17,65 @@ mod tls;
 use masa::Context;
 pub use tls::{client, server};
 
-#[cfg(not(feature = "masa"))]
+#[cfg(all(
+    not(feature = "masa"),
+    not(feature = "prio_global"),
+    not(feature = "prio_oldest"),
+    not(feature = "prio_local"),
+    not(feature = "fifo")
+))]
 #[allow(missing_docs)]
 pub type DefaultMasaHooks = noop::NoopMasaHooks;
 // pub type DefaultMasaHooks = queue_tracing::QueueTracing;
 
-#[cfg(all(feature = "fifo", feature = "early"))]
+#[cfg(all(
+    feature = "fifo",
+    feature = "early",
+    not(feature = "prio_global"),
+    not(feature = "prio_oldest"),
+    not(feature = "prio_local")
+))]
 #[allow(missing_docs)]
 pub type DefaultMasaHooks = fifo::Fifo;
 
-#[cfg(all(feature = "fifo", not(feature = "early")))]
+#[cfg(all(
+    feature = "fifo",
+    not(feature = "early"),
+    not(feature = "prio_global"),
+    not(feature = "prio_oldest"),
+    not(feature = "prio_local")
+))]
 #[allow(missing_docs)]
 // TODO: revert back to noop for Fifo. Add another feature flag for tracing.
 // pub type DefaultMasaHooks = noop::NoopMasaHooks;
 // pub type DefaultMasaHooks = tracing::Tracing;
 pub type DefaultMasaHooks = noop::NoopMasaHooks;
 
-#[cfg(any(feature = "fifo_span_tracing"))]
+#[cfg(all(
+    feature = "fifo_span_tracing",
+    not(feature = "prio_global"),
+    not(feature = "prio_oldest"),
+    not(feature = "prio_local")
+))]
 #[allow(missing_docs)]
 pub type DefaultMasaHooks = tracing::Tracing;
 
-#[cfg(any(feature = "fifo_queue_tracing"))]
+#[cfg(all(
+    feature = "fifo_queue_tracing",
+    not(feature = "prio_global"),
+    not(feature = "prio_oldest"),
+    not(feature = "prio_local")
+))]
 #[allow(missing_docs)]
 pub type DefaultMasaHooks = queue_tracing::QueueTracing;
 
 #[cfg(any(feature = "prio_global"))]
 #[allow(missing_docs)]
 pub type DefaultMasaHooks = queue_global::QueueGlobal;
+
+#[cfg(any(feature = "prio_oldest"))]
+#[allow(missing_docs)]
+pub type DefaultMasaHooks = prio_oldest::PrioOldest;
 
 #[cfg(any(feature = "prio_local"))]
 #[allow(missing_docs)]
---
+++ libs/tonic/tonic/src/masa/context/prio_oldest.rs
@@ -0,0 +1,180 @@
+use crate::{masa::context::read_context, GrpcMethod, Request, Status};
+use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
+use std::sync::Arc;
+use std::task::Poll;
+
+use super::super::{ClientHooks, MasaHooks, ParentHooks, ServerHooks};
+use crate::body::BoxBody;
+use crate::{Code, Response};
+use masa::{time_now, Context, ContextBuilder, EARLY_RETURN};
+
+#[derive(Debug)]
+/// This policy sets the priority of each child request to be the request generation time (prio_hint).
+#[allow(dead_code)]
+#[allow(unreachable_pub)]
+pub struct PrioOldest;
+
+impl MasaHooks for PrioOldest {
+    type ServerContext = ServerContext;
+    type ChildContext = ChildContext;
+    type ParentContext = ParentContext;
+}
+
+#[derive(Debug)]
+#[allow(unreachable_pub)]
+pub struct ServerContext {}
+
+impl ServerHooks for ServerContext {
+    fn new(_service_name: &'static str) -> Self {
+        Self {}
+    }
+}
+
+#[derive(Debug)]
+#[allow(unreachable_pub)]
+pub struct ParentContext {
+    ctx: Context,
+    q_lat: AtomicU64,
+    will_early_return: AtomicBool,
+}
+
+impl ParentContext {
+    #[inline]
+    fn check_early_return(&self) -> bool {
+        if EARLY_RETURN {
+            self.check_early_return_impl()
+        } else {
+            false
+        }
+    }
+
+    fn check_early_return_impl(&self) -> bool {
+        if self.will_early_return.load(Ordering::Relaxed) {
+            return true;
+        }
+
+        let now = time_now();
+        let should_early_return = now >= self.ctx.deadline();
+
+        if should_early_return {
+            if self
+                .will_early_return
+                .compare_exchange_weak(false, true, Ordering::Relaxed, Ordering::Relaxed)
+                .is_ok()
+            {}
+        }
+
+        return should_early_return;
+    }
+
+    #[inline]
+    fn issue_early_return(&self) -> Status {
+        Status::new(Code::DeadlineExceeded, format!("/EarlyReturn"))
+    }
+}
+
+impl ParentHooks<ChildContext, ServerContext> for ParentContext {
+    fn begin<B>(
+        _method: GrpcMethod,
+        req: &http::Request<B>,
+        _server_ctx: Arc<ServerContext>,
+    ) -> Self {
+        Self {
+            ctx: read_context(req),
+            q_lat: AtomicU64::new(0),
+            will_early_return: AtomicBool::new(false),
+        }
+    }
+
+    fn before_poll<Ret>(&self) -> Result<(), Result<Response<Ret>, Status>> {
+        if self.check_early_return() {
+            return Err(Err(self.issue_early_return()));
+        }
+
+        let queue_latency = tokio::task::obtain_task_queue_latency().as_micros() as u64;
+        if queue_latency > 0 {
+            self.q_lat.fetch_add(queue_latency, Ordering::AcqRel);
+        }
+        Ok(())
+    }
+
+    fn before_child_rpc<T>(
+        &self,
+        _child_method: GrpcMethod,
+        request: &mut Request<T>,
+        _child_ctx: &mut ChildContext,
+    ) -> Result<(), Status> {
+        if self.check_early_return() {
+            return Err(self.issue_early_return());
+        }
+
+        let deadline = self.ctx.deadline();
+        let prio_hint = self.ctx.prio_hint();
+
+        let child_recv_ctx = ContextBuilder::from(&self.ctx)
+            .deadline(deadline)
+            .prio_hint(prio_hint)
+            .build();
+        request.metadata_mut().insert_ctx("ctx", &child_recv_ctx);
+
+        Ok(())
+    }
+
+    fn after_child_rpc<T>(
+        &self,
+        _method: GrpcMethod,
+        response: &mut Result<Response<T>, Status>,
+        _child_ctx: ChildContext,
+    ) -> Result<(), Status> {
+        if let Ok(resp) = response {
+            if let Some(value) = resp
+                .metadata()
+                .get("x-queue-latency")
+                .or_else(|| resp.metadata().get("X-Queue-Latency"))
+            {
+                if let Ok(v) = value.to_str() {
+                    if let Ok(parsed) = v.parse::<u64>() {
+                        self.q_lat.fetch_add(parsed, Ordering::AcqRel);
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn after_poll<Ret>(
+        &self,
+        poll: &Poll<Result<Response<Ret>, Status>>,
+    ) -> Result<(), Result<Response<Ret>, Status>> {
+        match poll {
+            Poll::Pending => {
+                if self.check_early_return() {
+                    return Err(Err(self.issue_early_return()));
+                }
+            }
+            Poll::Ready(_) => {}
+        };
+
+        Ok(())
+    }
+
+    // expect frontend method, all other method are going send back their latency trace
+    fn finalize_after_serialization(&self, _response: &mut http::Response<BoxBody>) {
+        let res_header = _response.headers_mut();
+        let total = self.q_lat.load(Ordering::Acquire).to_string();
+        if let Ok(header_val) = http::HeaderValue::from_str(&total) {
+            // HTTP/2 metadata is lower-case; rely on hyper to canonicalize.
+            res_header.insert("x-queue-latency", header_val);
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+#[allow(unreachable_pub)]
+pub struct ChildContext {}
+
+impl ClientHooks for ChildContext {
+    fn new<T>(_method: GrpcMethod, _request: &Request<T>) -> Self {
+        Self {}
+    }
+}
