395bdbdbfacb2423e4d6d1441ac255b618f873f1
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2026-02-03 22:44:19 +0000

    feat: service name override for latency tracking
    Introduce a new header, `x-masa-service-name`, to allow for overriding
    the service name used in latency tracking. This is particularly useful
    for generic gRPC services that simulate multiple distinct services. The
    `EarlyReturnHandler` and various policy implementations have been
    updated to utilize this new override when available.
    
    Also, propagate the service name override in `mssim` and `synthetic`
    applications.

--- libs/tonic/tonic/src/masa/context/prio_oldest.rs
+++ libs/tonic/tonic/src/masa/context/prio_oldest.rs
@@ -2,9 +2,11 @@ use crate::{masa::context::read_context, GrpcMethod, Request, Status};
 use std::sync::Arc;
 use std::task::Poll;
 
+use super::super::common::{EarlyReturnHandler, QueueLatencyTracker};
 use super::super::{ClientHooks, MasaHooks, ParentHooks, ServerHooks};
-use super::common::{EarlyReturnHandler, QueueLatencyTracker};
-use super::{resolve_method_name, MasaRequestExt, METHOD_NAME_OVERRIDE_HEADER};
+use super::{
+    resolve_method_name, resolve_service_name, MasaRequestExt, METHOD_NAME_OVERRIDE_HEADER,
+};
 use crate::Response;
 use masa_core::{Context, ContextBuilder};
 
@@ -58,7 +60,7 @@ impl ParentHooks<ChildContext, ServerContext> for ParentContext {
             ctx: read_context(req),
             q_lat_tracker: QueueLatencyTracker::new(),
             early_return: EarlyReturnHandler::new(
-                method.service(),
+                resolve_service_name(method, req),
                 resolve_method_name(method, req),
             ),
         }
