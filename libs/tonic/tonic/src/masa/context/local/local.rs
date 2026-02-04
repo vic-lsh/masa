defc9c7af08fb41caf5dc8b951f42e200c45833f
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2026-02-04 05:04:44 +0000

    refactor: update ParentToChildId display formatting
    The `ParentToChildId` display format has been updated from
    `/{}/{}=>/{}/{}` to `{}::{}=>{}::{}` to improve readability and align
    with typical Rust naming conventions for services and methods.

--- libs/tonic/tonic/src/masa/context/local/local.rs
+++ libs/tonic/tonic/src/masa/context/local/local.rs
@@ -55,7 +55,7 @@ impl std::fmt::Display for ParentToChildId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "/{}/{}=>/{}/{}",
+            "{}::{}=>{}::{}",
             self.parent.service(),
             self.parent.method(),
             self.child.service(),
