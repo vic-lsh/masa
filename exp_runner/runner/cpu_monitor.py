d701b4718d48f245becd29c6b7efb996923d1e5c
vic-lsh <shli@cs.washington.edu>
2026-03-25 18:37:17 -0700

    chore: address lint errors

--- exp_runner/runner/cpu_monitor.py
+++ exp_runner/runner/cpu_monitor.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING, Any, Optional
 
 if TYPE_CHECKING:
-    from .executor import CommandExecutor
+    pass
 
 logger = logging.getLogger(__name__)
 
